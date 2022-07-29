002a9d2b87e54c3f18dd43bafdac88bafe2702e5
Andrew Stellwag <aestellwag@gmail.com>
2022-07-29 17:25:48 -0400

    resolved conflicts

--- plugin-flex-ts-template/src/flex-hooks/components/MainHeader.tsx
+++ plugin-flex-ts-template/src/flex-hooks/components/MainHeader.tsx
@@ -1,4 +1,6 @@
 import * as Flex from '@twilio/flex-ui';
+import { replaceUserControls } from '../../feature-library/activity-skill-filter/flex-hooks/components/MainHeader'
 
 export default (flex: typeof Flex, manager: Flex.Manager) => {
+  replaceUserControls(flex, manager);
 }
