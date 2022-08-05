072957dca909bf4d08e4413f39f81ab9095b889f
Jared Hunter <jhunter@twilio.com>
2022-08-05 11:28:56 -0500

    Tidy up some logic

--- plugin-flex-ts-template/src/flex-hooks/actions/AcceptTask.ts
+++ plugin-flex-ts-template/src/flex-hooks/actions/AcceptTask.ts
@@ -1,5 +1,6 @@
 import * as Flex from '@twilio/flex-ui';
-import TaskRouterService from 'utils/serverless/TaskRouter/TaskRouterService';
+import { Channel } from 'types/task-router';
+import TaskRouterService from '../../utils/serverless/TaskRouter/TaskRouterService';
 
 export default (flex: typeof Flex, manager: Flex.Manager) => {
   //beforeAcceptTask(flex, manager);
@@ -22,28 +23,39 @@ function omniChannelChatCapacityManager(flex: typeof Flex, manager: Flex.Manager
   
   Flex.Actions.addListener('afterAcceptTask', async (payload, abortFunction) => {
     const workerChanneslMap = manager.workerClient.channels;
-    const tasksMap = manager.store.getState().worker.tasks;
+    const tasksMap = manager.store.getState().flex.worker.tasks;
 
     const workerChannelsArray = Array.from(workerChanneslMap);
-    const chatChannel: any = workerChannelsArray.find((channel: any) => channel?.taskChannelUniqueName === 'chat');
-    const currentChatCapacity = chatChannel?.capacity;
+    const chatChannelArrayItem = workerChannelsArray.find(([string, channel]) => {
+      return channel?.taskChannelUniqueName === 'chat'});
+    const chatChannel = chatChannelArrayItem? chatChannelArrayItem[1] as Channel : null
+    
+    console.log("JARED1", chatChannel);
+    if(chatChannel) {
+      const currentChatCapacity = chatChannel.capacity;
+      const workerChannelSid = chatChannel.sid;
 
-    const tasksArray = Array.from(tasksMap);
-    const chatTasksArray = tasksArray.filter((task: any) => task.taskChannelUniqueName === 'chat');
+      const tasksArray = Array.from(tasksMap);
+      const chatTasksArray = tasksArray.filter((array: any) => array[1].taskChannelUniqueName === 'chat');
 
-    const workerSid = manager.workerClient.sid;
-    const workerChannelSid = chatChannel.sid;
+      console.log("JARED 1.5", tasksArray, chatTasksArray);
 
-    if (currentChatCapacity === 1) {
-        // we're assuming chat capacity has been artificially reduced
-        // reset it to the desired max value
-        TaskRouterService.updateWorkerChannel(workerSid, workerChannelSid, 2, true);
-    }
+      const workerSid = manager.workerClient.sid;
 
-    if (chatTasksArray.length > 1 && chatTasksArray.length === currentChatCapacity) {
-        // we're saturated
-        // reduce capacity on chat channel to 1
-        TaskRouterService.updateWorkerChannel(workerSid, workerChannelSid, 1, true);
+
+      if (currentChatCapacity === 1) {
+        console.log("JARED2");
+          // we're assuming chat capacity has been artificially reduced
+          // reset it to the desired max value
+          TaskRouterService.updateWorkerChannel(workerSid, workerChannelSid, 2, true);
+      }
+
+      if (chatTasksArray.length > 1 && chatTasksArray.length === currentChatCapacity) {
+        console.log("JARED3");
+          // we're saturated
+          // reduce capacity on chat channel to 1
+          TaskRouterService.updateWorkerChannel(workerSid, workerChannelSid, 1, true);
+      }
     }
 
     return;
