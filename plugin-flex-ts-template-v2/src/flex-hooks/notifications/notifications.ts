cc4f2f71d88a10ee85ab90968d12cbaff80af207
Jared Hunter <jhunter@twilio.com>
2022-10-17 16:25:49 -0500

    Adding missing reference

--- plugin-flex-ts-template-v2/src/flex-hooks/notifications/notifications.ts
+++ plugin-flex-ts-template-v2/src/flex-hooks/notifications/notifications.ts
@@ -3,10 +3,11 @@ import ActivityReservationHandler from "../../feature-library/activity-reservati
 import ActivitySkillFilter from "../../feature-library/activity-skill-filter/flex-hooks/notifications/ActivitySkillFilter";
 import Conference from "../../feature-library/conference/flex-hooks/notifications/Conference";
 import ChatTransfer from "../../feature-library/chat-transfer/flex-hooks/notifications/TransferResult";
+import TeamsViewfilters from "../../feature-library/teams-view-filters/flex-hooks/notifications/TeamViewQueueFilter"
 
 const notificationsToRegister: ((
   flex: typeof Flex,
   manager: Flex.Manager
-) => void)[] = [ActivityReservationHandler, ActivitySkillFilter, Conference, ChatTransfer];
+) => void)[] = [ActivityReservationHandler, ActivitySkillFilter, Conference, ChatTransfer, TeamsViewfilters];
 
 export default notificationsToRegister;
--- plugin-flex-ts-template-v2/src/flex-hooks/strings/strings.ts
+++ plugin-flex-ts-template-v2/src/flex-hooks/strings/strings.ts
@@ -2,7 +2,8 @@ import ActivityReservationHandler from "../../feature-library/activity-reservati
 import ActivitySkillFilter from "../../feature-library/activity-skill-filter/flex-hooks/strings/ActivitySkillFilter";
 import Conference from "../../feature-library/conference/flex-hooks/strings/Conference";
 import ChatTransfer from "../../feature-library/chat-transfer/flex-hooks/strings/ChatTransferStrings";
+import TeamsViewFilters from "../../feature-library/teams-view-filters/flex-hooks/strings/TeamViewQueueFilter";
 
-const customStrings = { ...ActivityReservationHandler, ...ActivitySkillFilter, ...Conference, ...ChatTransfer };
+const customStrings = { ...ActivityReservationHandler, ...ActivitySkillFilter, ...Conference, ...ChatTransfer, ...TeamsViewFilters };
 
 export default customStrings;
