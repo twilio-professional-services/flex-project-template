48b89e2ab0938c5127cbd4f5e32124d33cadb793
Sam Johnson <7373633+dremin@users.noreply.github.com>
2022-10-25 09:25:23 -0400

    Add multiple recording indicator options (#69)

---
+++ plugin-flex-ts-template-v2/src/feature-library/pause-recording/flex-hooks/components/CallCanvas.tsx
@@ -0,0 +1,15 @@
+import * as Flex from '@twilio/flex-ui';
+import PauseStatusPanel from '../../custom-components/PauseStatusPanel';
+import { UIAttributes } from 'types/manager/ServiceConfiguration';
+
+const { custom_data } = Flex.Manager.getInstance().serviceConfiguration.ui_attributes as UIAttributes;
+const { enabled = false, indicator_permanent = false } = custom_data?.features?.pause_recording || {}
+
+export function addPauseStatusPanel(flex: typeof Flex, manager: Flex.Manager) {
+  
+  if (!enabled || !indicator_permanent) return;
+
+  flex.CallCanvas.Content.add(<PauseStatusPanel key="pause-status-panel" />, {
+    sortOrder: -1
+  });
+}
