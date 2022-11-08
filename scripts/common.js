d663888d9e9c91f8545b237c707ce62e0339b0d2
Jared Hunter <jhunter@twilio.com>
2022-11-07 19:45:54 -0600

    refactoring scripts - testing yaml file

--- scripts/common.js
+++ scripts/common.js
@@ -1,23 +1,26 @@
-var shell = require("shelljs");
+const shell = require("shelljs");
 
 const originalPluginDir = 'plugin-flex-ts-template-v2';
 
+function getDirectoryName(originalName, regex) {
 
-var tempPluginDir = "";
+  var tempPluginDir = "";
 
-if(shell.test('-d', `${originalPluginDir}`)){
-  tempPluginDir = originalPluginDir;
-} else {
-  shell.ls('./').forEach(function (dir){
-    if(shell.test('-d', dir)){
-      if(dir.match(/flex-template-.*/)){
-        tempPluginDir  = dir;
+  if(shell.test('-d', `${originalName}`)){
+    tempPluginDir = originalName;
+  } else {
+    shell.ls('./').forEach(function (dir){
+      if(shell.test('-d', dir)){
+        if(dir.match(regex)){
+          tempPluginDir  = dir;
+        }
       }
-    }
-  })
+    })
+  }
+  return tempPluginDir;
 }
 
-const pluginDir = tempPluginDir;
+const pluginDir = getDirectoryName(originalPluginDir, /flex-template-.*/);
 const serverlessDir = 'serverless-functions';
 const scheduleManagerServerlessDir = 'serverless-schedule-manager';
 const flexConfigDir = 'flex-config';
@@ -27,6 +30,273 @@ const pluginSrc = `${pluginDir}/src`;
 const serverlessSrc = `${serverlessDir}/src`;
 const flexConfigTemplateDir = `${flexConfigDir}/template-files`
 
+// the following function will use twilio cli
+// to try to fetch the sids of template dependencies based on typical 
+// dependency names.  
+
+// It will always return an object and if its been succesfull the sids will
+// be populated on the return object
+
+// this function is expected to run with either an active profile set
+// or with the following environment variables set
+//    TWILIO_ACCOUNT_SID, 
+//    TWILIO_API_KEY, 
+//    TWILIO_API_SECRET
+exports.getEnvironmentVariables = function getEnvironmentVariables() {
+
+  var result = {};
+
+  try {
+    const FLEX_WORKSPACE_NAME = "Flex Task Assignment";
+    const SYNC_SERVICE_NAME = "Default Service";
+    const CHAT_SERVICE_NAME = /(Flex.*Service)/
+
+    const ANYONE_WORKFLOW_NAME = /(Assign.*Anyone)/
+    const CHAT_WORRKFLOW_NAME = "Chat Transfer"
+    const CALLBACK_WORKFLOW_NAME = "Callback"
+    const INTERNAL_CALL_WORKFLOW_NAME = "Internal Call";
+
+    const SERVERLESS_FUNCTIONS_SERVICE_NAME = "custom-flex-extensions-serverless"
+    const SCHEDULE_MANAGER_SERVICE_NAME = "schedule-manager"
+
+
+    console.log("Loading environment variables..");
+    console.log("");
+
+    result.taskrouter_workspace_sid = shell.exec("twilio api:taskrouter:v1:workspaces:list", {silent: true}).grep(FLEX_WORKSPACE_NAME).stdout.split(" ")[0]
+    result.sync_sid = shell.exec("twilio api:sync:v1:services:list", {silent: true}).grep(SYNC_SERVICE_NAME).stdout.split(" ")[0]
+    result.chat_sid = shell.exec("twilio api:chat:v2:services:list", {silent: true}).grep(CHAT_SERVICE_NAME).stdout.split(" ")[0]
+
+    var workflows = shell.exec(`twilio api:taskrouter:v1:workspaces:workflows:list --workspace-sid=${result.taskrouter_workspace_sid}`, {silent: true});
+    result.everyoneWorkflow = workflows.grep(ANYONE_WORKFLOW_NAME).stdout.split(" ")[0].trim();
+    result.chatTransferWorkFlow = workflows.grep(CHAT_WORRKFLOW_NAME).stdout.split(" ")[0].trim();
+    result.callbackWorkflow = workflows.grep(CALLBACK_WORKFLOW_NAME).stdout.split(" ")[0].trim();
+    result.internalCallWorkflow = workflows.grep(INTERNAL_CALL_WORKFLOW_NAME).stdout.split(" ")[0].trim();
+
+    result.serviceFunctionsSid = shell.exec("twilio api:serverless:v1:services:list", {silent: true}).grep(SERVERLESS_FUNCTIONS_SERVICE_NAME).stdout.split(" ")[0]
+    result.serviceFunctionsDomain = shell.exec(`twilio api:serverless:v1:services:environments:list --service-sid=${result.serviceFunctionsSid}`, {silent: true}).grep(SERVERLESS_FUNCTIONS_SERVICE_NAME).stdout.split(" ")[4]
+
+    result.scheduleFunctionsSid = shell.exec("twilio api:serverless:v1:services:list", {silent: true}).grep(SCHEDULE_MANAGER_SERVICE_NAME).stdout.split(" ")[0]
+    result.scheduledFunctionsDomain = shell.exec(`twilio api:serverless:v1:services:environments:list --service-sid=${result.scheduleFunctionsSid}`, {silent: true}).grep(SCHEDULE_MANAGER_SERVICE_NAME).stdout.split(" ")[4]
+
+    return result;
+
+  } catch (error) {
+    console.warn("Error trying to load environment variables");
+    console.error(error);
+    return result;
+  }
+}
+
+exports.getActiveTwilioProfile = async function getActiveTwilioProfile() {
+
+  var result = {};
+  
+  try{
+    result.profile = await shell.exec("twilio profiles:list", {silent: true}).grep("true").stdout;
+    result.profile_name = result.profile.split(" ")[0].trim().trimStart();
+    result.account_sid = result.profile.split("  ")[1].trim().trimStart();
+
+    return result;
+
+  } catch (error) {
+    console.warn("Unable to detect active twilio profile");
+    console.log("");
+    return result;
+  }
+
+}
+
+exports.installNPMServerlessFunctions = function installNPMServerlessFunctions() {
+  console.log("Installing npm dependencies for serverless functions..");
+  shell.exec("npm --prefix ./serverless-functions ci ./serverless-functions", {silent:true});
+}
+
+exports.installNPMFlexConfig = function installNPMFlexConfig() {
+  console.log("Installing npm dependencies for flex-config...");
+  shell.exec("npm --prefix ./flex-config ci ./flex-config", {silent:true});
+}
+
+exports.installNPMPluginV2 = function installNPMPluginV2() {
+  console.log(`Installing npm dependencies for ${pluginDir}...`);
+  shell.exec(`npm --prefix ./${pluginDir} ci ./${pluginDir}`, {silent:true});
+}
+
+exports.generateServerlessFunctionsEnv = function generateServerlessFunctionsEnv(context, serverlessEnv) {
+
+  try {
+    const serverlessEnvExample = `./${serverlessDir}/.env.example`;
+    var {
+      account_sid, 
+      auth_token, 
+      api_key, 
+      api_secret, 
+      taskrouter_workspace_sid, 
+      sync_sid, 
+      chat_sid, 
+      callbackWorkflow, 
+      everyoneWorkflow, 
+      internalCallWorkflow, 
+      chatTransferWorkFlow } = context
+
+    if(process.env.TWILIO_API_KEY && !api_key) api_key = process.env.TWILIO_API_KEY;
+    if(process.env.TWILIO_API_SECRET && !api_secret) api_secret = process.env.TWILIO_API_SECRET;
+
+    if(!shell.test('-e', serverlessEnv)){
+      shell.cp(serverlessEnvExample, serverlessEnv);
+    }
+    if(shell.test('-e', serverlessEnv)){
+      if(account_sid){
+        shell.sed('-i', /<YOUR_TWILIO_ACCOUNT_SID>/g, `${account_sid}`, serverlessEnv);
+      }
+      if(auth_token){
+        shell.sed('-i', /<YOUR_TWILIO_AUTH_TOKEN>/g, `${auth_token}`, serverlessEnv);
+      }
+      if(api_key){
+        shell.sed('-i', /<YOUR_API_KEY>/g, `${api_key}`, serverlessEnv);
+      }
+      if(api_secret){
+        shell.sed('-i', /<YOUR_API_SECRET>/g, `${api_secret}`, serverlessEnv);
+      }
+      if(taskrouter_workspace_sid){
+        shell.sed('-i', /<YOUR_FLEX_WORKSPACE_SID>/g, `${taskrouter_workspace_sid}`, serverlessEnv);
+      }
+      if(sync_sid){
+        shell.sed('-i', /<YOUR_FLEX_SYNC_SID>/g, `${sync_sid}`, serverlessEnv);
+      }
+      if(chat_sid){
+        shell.sed('-i', /<YOUR_FLEX_CHAT_SERVICE_SID>/g, `${chat_sid}`, serverlessEnv);
+      }
+
+      if(callbackWorkflow || everyoneWorkflow){
+        var workflow = callbackWorkflow?  callbackWorkflow: everyoneWorkflow;
+        shell.sed('-i', /<YOUR_FLEX_CALLBACK_WORKFLOW_SID>/g, `${workflow}`, serverlessEnv);
+      }
+      if(internalCallWorkflow){
+        shell.sed('-i', /<YOUR_FLEX_INTERNAL_CALL_WORKFLOW_SID>/g, `${internalCallWorkflow}`, serverlessEnv);
+      }
+      if(chatTransferWorkFlow){
+        shell.sed('-i', /<YOUR_FLEX_CHAT_TRANSFER_WORKFLOW_SID>/g, `${chatTransferWorkFlow}`, serverlessEnv);
+      }
+      console.log(`Setting up environment ${serverlessEnv}: complete`);
+    } else {
+      console.warn("Unable to configure serverless environment file, it will need to be done manually");
+    }
+  } catch (error) {
+    console.error(error);
+    console.log(`Error attempting to generate serverless environment file ${serverlessEnv}`);
+  }
+  console.log("");
+}
+
+exports.generateFlexConfigEnv = function generateFlexConfigEnv(context, flexConfigEnv) {
+
+  const flexConfigEnvExample = `./${flexConfigDir}/.env.example`;
+  const { 
+    account_sid,
+    api_key,
+    api_secret } = context
+
+  try {
+
+    // create file from example if it does not exist  
+    if(!shell.test('-e', flexConfigEnv)){
+      shell.cp(flexConfigEnvExample, flexConfigEnv);
+    }
+
+    // confirm file exists and perform replacements with
+    // variables when available
+    if(shell.test('-e', flexConfigEnv)){
+      if(account_sid){
+        shell.sed('-i', /<YOUR_TWILIO_ACCOUNT_SID>/g, `${account_sid}`, flexConfigEnv);
+      }
+      if(api_key){
+        shell.sed('-i', /<YOUR_API_KEY>/g, `${api_key}`, flexConfigEnv);
+      } 
+      if(api_secret){
+        shell.sed('-i', /<YOUR_API_SECRET>/g, `${api_secret}`, flexConfigEnv);
+      }
+      console.log(`"Setting up environment ${flexConfigEnv}: complete`);
+    } else {
+      console.warn("Unable to configure flex config environment file, it will need to be done manually");
+    }
+  } catch (error){
+    console.error(error);
+    console.log(`Error attempting to generate flex config environment file ${flexConfigEnv}`);
+  }
+  console.log("");
+}
+
+exports.populateFlexConfigPlaceholders = function populateFlexConfigPlaceholders(context, environment) {
+
+  const configFile = `./${flexConfigDir}/ui_attributes.${environment}.json`;
+
+  var {
+    scheduledFunctionsDomain,
+    serviceFunctionsDomain
+     } = context
+
+    if(shell.test('-e', configFile)){
+      if(serviceFunctionsDomain){
+        shell.sed('-i', /<PLACEHOLDER_SERVERLESS_DOMAIN>/g, `${serviceFunctionsDomain}`, configFile);
+      }
+      if(serviceFunctionsDomain){
+        shell.sed('-i', /<PLACEHOLDER_SCHEDULE_MANAGER_DOMAIN>/g, `${scheduledFunctionsDomain}`, configFile);
+      }
+    }
+}
+
+exports.generateAppConfigForPluginV2 = function generateAppConfigForPluginV2() {
+
+  const pluginAppConfigExample = `./${pluginDir}/public/appConfig.example.js`
+  const pluginAppConfig = `./${pluginDir}/public/appConfig.js`
+
+  try{
+
+    if(!shell.test('-e', pluginAppConfig)){
+      shell.cp(pluginAppConfigExample, pluginAppConfig);
+    }
+    console.log("Setting up public/appConfig.js: complete");
+  } catch (error) {
+    console.error(error);
+    console.log(`Error attempting to generate appConfig file ${pluginAppConfig}`);
+  }
+  console.log("");
+}
+
+exports.printEnvironmentSummary = function printEnvironmentSummary(context){
+
+  const {
+    taskrouter_workspace_sid, 
+    sync_sid, 
+    chat_sid, 
+    callbackWorkflow, 
+    everyoneWorkflow, 
+    internalCallWorkflow, 
+    chatTransferWorkFlow,
+    serviceFunctionsDomain,
+    scheduledFunctionsDomain } = context
+
+    console.log("Environment configuration summary");
+    console.log("");
+    console.log("---- INSTANCE SIDS -----------------------------------------");
+    console.log("Taskrouter Flex Workspace sid: \t\t", taskrouter_workspace_sid);
+    console.log("Default Sync sid: \t\t\t", sync_sid);
+    console.log("chat/conversation service sid: \t\t", chat_sid);
+    console.log("");
+    console.log("---- WORKFLOW SIDS -----------------------------------------");
+    console.log("Assign to Anyone workflow sid: \t\t", everyoneWorkflow);
+    console.log("chat transfer workflow sid: \t\t", chatTransferWorkFlow);
+    console.log("callback workflow sid: \t\t\t", callbackWorkflow);
+    console.log("internal call workflow sid: \t\t", internalCallWorkflow);
+    console.log("");
+    console.log("---- SERVERLESS DOMAINS -----------------------------------------");
+    console.log("serverless functions: \t\t\t", serviceFunctionsDomain);
+    console.log("schedule manager: \t\t\t", scheduledFunctionsDomain);
+    console.log("");
+    console.log("");
+    console.log("");
+}
 
 
 exports.pluginDir = pluginDir;
@@ -38,3 +308,4 @@ exports.featureDirectory = featureDirectory;
 exports.pluginSrc = pluginSrc;
 exports.serverlessSrc = serverlessSrc;
 exports.flexConfigTemplateDir = flexConfigTemplateDir;
+
--- scripts/setup-local-environment.js
+++ scripts/setup-local-environment.js
@@ -1,42 +1,29 @@
-const { pluginDir, flexConfigDir, serverlessDir } = require ('./common');
+const { flexConfigDir, serverlessDir, getEnvironmentVariables, getActiveTwilioProfile, installNPMServerlessFunctions, generateServerlessFunctionsEnv, printEnvironmentSummary, installNPMFlexConfig, generateFlexConfigEnv, installNPMPluginV2, generateAppConfigForPluginV2 } = require ('./common');
 const shell = require("shelljs");
 const prompt = require('prompt');
 // https://github.com/shelljs/shelljs#shellstringstr
 
 
-var profile;
-var profile_name;
-var account_sid;
-var auth_token;
-var taskrouter_workspace_sid;
-var sync_sid;
-var chat_sid;
-var api_key;
-var api_secret;
-var everyoneWorkflow;
-var chatTransferWorkFlow;
-var callbackWorkflow;
-var internalCallWorkflow;
 
-var serviceFunctionsSid
-var serviceFunctionsDomain
-var scheduleFunctionsSid
-var scheduledFunctionsDomain
-
-var serverlessEnvExample = `./${serverlessDir}/.env.example`;
 var serverlessEnv = `./${serverlessDir}/.env`;
-var flexConfigEnvExample = `./${flexConfigDir}/.env.example`;
 var flexConfigEnv = `./${flexConfigDir}/.env`;
-var pluginAppConfigExample = `./${pluginDir}/public/appConfig.example.js`
-var pluginAppConfig = `./${pluginDir}/public/appConfig.js`
 
 const installNpm = process.argv[2]? process.argv[2] == false: true;
 
 console.log(" ----- START OF POST INSTALL SCRIPT ----- ");
 console.log("");
 
-setActiveProfile().then(() => {
+var context;
 
+getActiveTwilioProfile().then((profile_result) => {
+
+  context = { ...profile_result }
+  const { profile_name, account_sid } = context;
+
+  // define the prompt for local environment setup
+  // and account_sid from the detected profile and the auth token are required to 
+  // properly populate the serverless env file for running locally
+  // the prompt will skip if an account sid has not been detected
   var prompt_schema  = {
     properties: {
       proceed: {
@@ -47,27 +34,6 @@ setActiveProfile().then(() => {
         required: true,
         ask: function() {
           return account_sid;
-        }
-      },
-      api_key: {
-        description: "Please enter the API KEY for your Twilio Account",
-        pattern: /^SK[a-zA-Z0-9]+$/,
-        message: 'API Key/SID must start with SK and be made up of only numbers and letters',
-        required: true,
-        ask: function() {
-          return false;
-          return prompt.history('proceed') && prompt.history('proceed').value.toLocaleLowerCase() === 'y';
-        }
-      },
-      api_secret: {
-        description: "Please enter the API Secret for your Twilio Account",
-        message: 'API Secret must be present',
-        hidden: true,
-        replace: '*',
-        required: true,
-        ask: function() {
-          return false;
-          return prompt.history('proceed') && prompt.history('proceed').value.toLocaleLowerCase() === 'y';
         }
       },
       auth_token: {
@@ -84,216 +50,53 @@ setActiveProfile().then(() => {
   };
 
   prompt.start();
-  prompt.get(prompt_schema, function(err, result) {
-    if(result.proceed && result.proceed.toLowerCase().includes("n"))
+  prompt.get(prompt_schema, function(err, prompt_result) {
+  
+    // we will abandon the script if you have expressly said no or 
+    // an account was not detected
+    if(!prompt_result.proceed || prompt_result.proceed.toLowerCase().includes("n"))
     {
         console.log("");
-        console.log("Ok, abandoning script.  Please run \"npm run postinstall\" when the correct profile is set");
+        console.log("Ok, abandoning local environment setup script.  Please run \"npm run postinstall\" when the correct profile is set");
         return; 
     }
     else {
-      auth_token = result.auth_token
-      api_key = result.api_key
-      api_secret = result.api_secret
       
-      if(account_sid){
-        setEnvironmentVariables();
+      context = { 
+        ...context, 
+        ...prompt_result, 
+        ...getEnvironmentVariables()
+      };
+
+      if(installNpm){
+        installNPMServerlessFunctions();
+        installNPMFlexConfig();
+        installNPMPluginV2();
       }
-      setupServerlessFunctions();
-      setupFlexConfig();
-      setupPlugin();
-      postInstallInstructions();
-      return;
+
+      if(context.account_sid) {
+        generateServerlessFunctionsEnv(context, serverlessEnv);
+        generateFlexConfigEnv(context, flexConfigEnv);
+        generateAppConfigForPluginV2();
+
+        printEnvironmentSummary(context);
+
+        console.log("if there are missing workflow sids, you can set those up for those features manually later");
+        console.log("You can now run the following command to start you local serverless functions and flex plugin together")
+        console.log("$ npm run start:local");
+      } else {
+        console.log("*****     WARNING       *****");
+        console.log(`Twilio cli profile not detected, please set an active profile with`);
+        console.log(`twilio profiles:use <profile-name>`);
+        console.log(`or populate the ${serverlessEnv} and ${flexConfigEnv} files with the required account sids manually`);
+        console.log("");
+        console.log("Once you have setup the environment you can run the following command to start you local serverless functions and flex plugin together")
+        console.log("$ npm run start:local");
+      }
+
+      console.log("");
+      console.log(" ----- END OF POST INSTALL SCRIPT ----- ");
+      console.log("");
     }
   });
 });
-
-async function setActiveProfile() {
-
-  try{
-    profile = await shell.exec("twilio profiles:list", {silent: true}).grep("true").stdout;
-    profile_name = profile.split(" ")[0].trim().trimStart();
-    account_sid = profile.split("  ")[1].trim().trimStart();
-
-  } catch (error) {
-    console.warn("Unable to detect active twilio profile, environment configuration files will have to be setup manually");
-    console.log("");
-  }
-
-}
-
-function setEnvironmentVariables() {
-
-  try {
-    console.log("Loading environment variables..");
-    console.log("");
-    
-    taskrouter_workspace_sid = shell.exec("twilio api:taskrouter:v1:workspaces:list", {silent: true}).grep("Flex Task Assignment").stdout.split(" ")[0]
-    sync_sid = shell.exec("twilio api:sync:v1:services:list", {silent: true}).grep("Default Service").stdout.split(" ")[0]
-    chat_sid = shell.exec("twilio api:chat:v2:services:list", {silent: true}).grep("Flex").grep("Service").stdout.split(" ")[0]
-
-    var workflows = shell.exec(`twilio api:taskrouter:v1:workspaces:workflows:list --workspace-sid=${taskrouter_workspace_sid}`, {silent: true});
-    everyoneWorkflow = workflows.grep("Assign").grep("Anyone").stdout.split(" ")[0].trim();
-    chatTransferWorkFlow = workflows.grep("Chat Transfer").stdout.split(" ")[0].trim();
-    callbackWorkflow = workflows.grep("Callback").stdout.split(" ")[0].trim();
-    internalCallWorkflow = workflows.grep("Internal Call").stdout.split(" ")[0].trim();
-
-    serviceFunctionsSid = shell.exec("twilio api:serverless:v1:services:list", {silent: true}).grep("custom-flex-extensions-serverless").stdout.split(" ")[0]
-    serviceFunctionsDomain = shell.exec(`twilio api:serverless:v1:services:environments:list --service-sid=${serviceFunctionsSid}`, {silent: true}).grep("custom-flex-extensions-serverless").stdout.split(" ")[4]
-
-    scheduleFunctionsSid = shell.exec("twilio api:serverless:v1:services:list", {silent: true}).grep("schedule-manager").stdout.split(" ")[0]
-    scheduledFunctionsDomain = shell.exec(`twilio api:serverless:v1:services:environments:list --service-sid=${scheduleFunctionsSid}`, {silent: true}).grep("schedule-manager").stdout.split(" ")[4]
-
-
-  } catch (error) {
-    console.warn("Error trying to load environment variables, environment configuration files will have to be setup manually");
-  }
-}
-
-function setupServerlessFunctions() {
-  if(installNpm){
-    console.log("Installing npm dependencies for serverless functions..");
-    shell.exec("npm --prefix ./serverless-functions ci ./serverless-functions", {silent:true});
-  }
-  if(!shell.test('-e', serverlessEnv)){
-    shell.cp(serverlessEnvExample, serverlessEnv);
-  }
-  if(shell.test('-e', serverlessEnv)){
-    if(account_sid){
-      shell.sed('-i', /<YOUR_TWILIO_ACCOUNT_SID>/g, `${account_sid}`, serverlessEnv);
-    }
-    if(auth_token){
-      shell.sed('-i', /<YOUR_TWILIO_AUTH_TOKEN>/g, `${auth_token}`, serverlessEnv);
-    }
-    if(api_key){
-      shell.sed('-i', /<YOUR_API_KEY>/g, `${api_key}`, serverlessEnv);
-    }
-    if(api_secret){
-      shell.sed('-i', /<YOUR_API_SECRET>/g, `${api_secret}`, serverlessEnv);
-    }
-    if(taskrouter_workspace_sid){
-      shell.sed('-i', /<YOUR_FLEX_WORKSPACE_SID>/g, `${taskrouter_workspace_sid}`, serverlessEnv);
-    }
-    if(sync_sid){
-      shell.sed('-i', /<YOUR_FLEX_SYNC_SID>/g, `${sync_sid}`, serverlessEnv);
-    }
-    if(chat_sid){
-      shell.sed('-i', /<YOUR_FLEX_CHAT_SERVICE_SID>/g, `${chat_sid}`, serverlessEnv);
-    }
-
-    if(callbackWorkflow || everyoneWorkflow){
-      var workflow = callbackWorkflow?  callbackWorkflow: everyoneWorkflow;
-      shell.sed('-i', /<YOUR_FLEX_CALLBACK_WORKFLOW_SID>/g, `${workflow}`, serverlessEnv);
-    }
-    if(internalCallWorkflow){
-      shell.sed('-i', /<YOUR_FLEX_INTERNAL_CALL_WORKFLOW_SID>/g, `${internalCallWorkflow}`, serverlessEnv);
-    }
-    if(chatTransferWorkFlow){
-      shell.sed('-i', /<YOUR_FLEX_CHAT_TRANSFER_WORKFLOW_SID>/g, `${chatTransferWorkFlow}`, serverlessEnv);
-    }
-    console.log("Setting up serverless-functions environment file: complete");
-  } else {
-    console.warn("Unable to configure serverless environment file, it will need to be done manually");
-  }
-  console.log("");
-}
-
-function setupFlexConfig() {
-  if(installNpm){
-    console.log("Installing npm dependencies for flex-config...");
-    shell.exec("npm --prefix ./flex-config ci ./flex-config", {silent:true});
-  }
-  if(!shell.test('-e', flexConfigEnv)){
-    shell.cp(flexConfigEnvExample,flexConfigEnv);
-  }
-  if(shell.test('-e', flexConfigEnv)){
-    if(account_sid){
-      shell.sed('-i', /<YOUR_TWILIO_ACCOUNT_SID>/g, `${account_sid}`, flexConfigEnv);
-    }
-    if(api_key){
-      shell.sed('-i', /<YOUR_API_KEY>/g, `${api_key}`, flexConfigEnv);
-    } 
-    if(api_secret){
-      shell.sed('-i', /<YOUR_API_SECRET>/g, `${api_secret}`, flexConfigEnv);
-    }
-    console.log("Setting up flex-config environment file: complete");
-  } else {
-    console.warn("Unable to configure flex config environment file, it will need to be done manually");
-  }
-  console.log("");
-}
-
-function setupPlugin() {
-  if(installNpm){
-    console.log(`Installing npm dependencies for ${pluginDir}...`);
-    shell.exec(`npm --prefix ./${pluginDir} ci ./${pluginDir}`, {silent:true});
-  }
-  if(!shell.test('-e', pluginAppConfig)){
-    shell.cp(pluginAppConfigExample, pluginAppConfig);
-  }
-  console.log("Setting up public/appConfig.js: complete");
-  console.log("");
-}
-
-function postInstallInstructions(){
-
-  var regex_apisecret = new RegExp(`${api_secret}`, 'g');
-  var regex_authtoken = new RegExp(`${auth_token}`, 'g');
-
-  if(prompt.history('proceed') && prompt.history('proceed').value.toLocaleLowerCase() === 'y'){
-    console.log("The environment configuration for serverless and flex-config was populated with the following environment variables");
-
-    console.log("");
-    console.log("---- INSTANCE SIDS -----------------------------------------");
-    console.log("Taskrouter Flex Workspace sid: \t\t", taskrouter_workspace_sid);
-    console.log("Default Sync sid: \t\t\t", sync_sid);
-    console.log("chat/conversation service sid: \t\t", chat_sid);
-    console.log("");
-    console.log("---- WORKFLOW SIDS -----------------------------------------");
-    console.log("Assign to Anyone workflow sid: \t\t", everyoneWorkflow);
-    console.log("chat transfer workflow sid: \t\t", chatTransferWorkFlow);
-    console.log("callback workflow sid: \t\t\t", callbackWorkflow);
-    console.log("internal call workflow sid: \t\t", internalCallWorkflow);
-    console.log("");
-    console.log("---- SERVERLESS DOMAINS -----------------------------------------");
-    console.log("serverless functions: \t\t\t", serviceFunctionsDomain);
-    console.log("schedule manager: \t\t\t", scheduledFunctionsDomain);
-    console.log("");
-    console.log("");
-    console.log("if there are missing workflow sids, you can set those up for those features manually later");
-
-    console.log("");
-    console.log("You can now run the following command to start you local serverless functions and flex plugin together")
-  
-  } else {
-    console.log("*****     WARNING       *****");
-    console.log(`Twilio cli profile not detected, please populate the ${serverlessEnv} and ${flexConfigEnv} files with the required account sids manually`);
-    console.log("");
-    console.log("Once you have setup the environment you can run the following command to start you local serverless functions and flex plugin together")
-  }
-
-  console.log("$ npm run start:local");
-  console.log("");
-  console.log(" ----- END OF POST INSTALL SCRIPT ----- ");
-  console.log("");
-
-  if(false){
-    console.log("");
-    console.log("");
-    console.log("-------- START FLEX CONFIG --------- ");
-    console.log("");
-    console.log(shell.cat(`${flexConfigEnv}`).sed('-i', regex_apisecret, "**********").sed('-i', regex_authtoken, "**********").stdout);
-    console.log("");
-    console.log("-------- END FLEX CONFIG --------- ");
-    console.log("");
-    console.log("");
-    console.log("");
-    console.log("-------- START SERVERLESS CONFIG --------- ");
-    console.log("");
-    console.log(shell.cat(`${serverlessEnv}`).sed('-i', regex_apisecret, "**********").sed('-i', regex_authtoken, "**********").stdout);
-    console.log("");
-    console.log("-------- END SERVERLESS CONFIG --------- ");
-
-  }
-
-}
