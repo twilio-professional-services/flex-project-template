84010ca895e1a3899565c3ffcc40d38f606049c5
Jared Hunter <jhunter@twilio.com>
2022-12-14 11:48:50 -0600

    Removing relationship with SupervisorBargeCoach

--- plugin-flex-ts-template-v2/src/feature-library/supervisor-complete-reservation/flex-hooks/components/SupervisorTaskCanvasHeader.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/supervisor-complete-reservation/flex-hooks/components/SupervisorTaskCanvasHeader.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as Flex from '@twilio/flex-ui';
 import SupervisorCompleteReservation from '../../custom-components/SupervisorCompleteReservation'
-import { isFeatureEnabled, isSupervisorBargeCoachEnabled } from '../..';
+import { isFeatureEnabled } from '../..';
 
 export function addUpdateReservationToSupervisorTaskCanvasHeader(flex: typeof Flex) {
 
-  if(!isFeatureEnabled() || isSupervisorBargeCoachEnabled()) return;
+  if(!isFeatureEnabled()) return;
 
     flex.Supervisor.TaskCanvasHeader.Content.add(<SupervisorCompleteReservation key="supervisor-complete-reservation"/>,
         {  sortOrder: 3, align: "end" }
