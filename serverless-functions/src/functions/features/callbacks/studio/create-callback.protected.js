7e6581e26a2a83775b07f2be53384ada6b9b39ab
Lindsay Hamoudi <lhamoudi@twilio.com>
2023-01-27 09:42:03 -0800

    Removing `flexFlowSid` as a required param from both `create-callback` functions (as this could be called from TwiML app instead of a Studio Flow). And catering to Transcription Callback events being the invokers of `create-callback` function (i.e. slightly different parameter naming convention). Updated README to reflect optional `flexFlowSid`, and give description as to how it's used

--- serverless-functions/src/functions/features/callbacks/studio/create-callback.protected.js
+++ serverless-functions/src/functions/features/callbacks/studio/create-callback.protected.js
@@ -1,4 +1,6 @@
-const { prepareStudioFunction } = require(Runtime.getFunctions()["common/helpers/prepare-function"].path);
+const { prepareStudioFunction } = require(Runtime.getFunctions()[
+  "common/helpers/prepare-function"
+].path);
 const TaskOperations = require(Runtime.getFunctions()[
   "common/twilio-wrappers/taskrouter"
 ].path);
@@ -9,78 +11,90 @@ const requiredParameters = [
     key: "numberToCallFrom",
     purpose: "the number to call the customer from",
   },
-  {
-    key: "flexFlowSid",
-    purpose: "the SID of the Flex Flow that triggered this function",
-  },
 ];
 
-exports.handler = prepareStudioFunction(requiredParameters, async (context, event, callback, response, handleError) => {
-  try {
-    const {
-      numberToCall,
-      numberToCallFrom,
-      flexFlowSid,
-      workflowSid: overriddenWorkflowSid,
-      timeout: overriddenTimeout,
-      priority: overriddenPriority,
-      attempts: retryAttempt,
-      conversation_id,
-      message,
-      utcDateTimeReceived,
-      recordingSid,
-      recordingUrl,
-      transcriptSid,
-      transcriptText,
-      isDeleted,
-      taskChannel: overriddenTaskChannel,
-    } = event;
-    
-    // use assigned values or use defaults
-    const workflowSid =
-      overriddenWorkflowSid || process.env.TWILIO_FLEX_CALLBACK_WORKFLOW_SID;
-    const timeout = overriddenTimeout || 86400;
-    const priority = overriddenPriority || 0;
-    const attempts = retryAttempt || 0;
-    const taskChannel = overriddenTaskChannel || "voice";
-    
-    // setup required task attributes for task
-    const attributes = {
-      taskType: recordingSid ? "voicemail" : "callback",
-      name: (recordingSid ? "Voicemail" : "Callback") + ` (${numberToCall})`,
-      flow_execution_sid: flexFlowSid,
-      message: message || null,
-      callBackData: {
+exports.handler = prepareStudioFunction(
+  requiredParameters,
+  async (context, event, callback, response, handleError) => {
+    try {
+      const {
         numberToCall,
         numberToCallFrom,
-        attempts,
-        mainTimeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-        utcDateTimeReceived: utcDateTimeReceived || new Date(),
-        recordingSid,
-        recordingUrl,
-        transcriptSid,
-        transcriptText,
-        isDeleted: isDeleted || false,
-      },
-      direction: "inbound",
-      conversations: {
+        flexFlowSid,
+        workflowSid: overriddenWorkflowSid,
+        timeout: overriddenTimeout,
+        priority: overriddenPriority,
+        attempts: retryAttempt,
         conversation_id,
-      },
-    };
-    
-    const result = await TaskOperations.createTask({
-      context,
-      workflowSid,
-      taskChannel,
-      attributes,
-      priority,
-      timeout,
-      attempts: 0,
-    });
-    response.setStatusCode(result.status);
-    response.setBody({ success: result.success, taskSid: result.taskSid });
-    callback(null, response);
-  } catch (error) {
-    handleError(error);
+        message,
+        utcDateTimeReceived,
+        recordingSid,
+        RecordingSid,
+        recordingUrl,
+        RecordingUrl,
+        transcriptSid,
+        TranscriptionSid,
+        transcriptText,
+        TranscriptionText,
+        isDeleted,
+        taskChannel: overriddenTaskChannel,
+      } = event;
+
+      // use assigned values or use defaults
+      const workflowSid =
+        overriddenWorkflowSid || process.env.TWILIO_FLEX_CALLBACK_WORKFLOW_SID;
+      const timeout = overriddenTimeout || 86400;
+      const priority = overriddenPriority || 0;
+      const attempts = retryAttempt || 0;
+      const taskChannel = overriddenTaskChannel || "voice";
+
+      // use explicitly passed in values or use values from the transcription callback event (if applicable)
+      const definitiveRecordingSid = recordingSid || RecordingSid;
+      const definitiveRecordingUrl = recordingUrl || RecordingUrl;
+      const definitiveTranscriptionSid = transcriptSid || TranscriptionSid;
+      const definitiveTranscriptionText =
+        transcriptText || TranscriptionText;
+
+      // setup required task attributes for task
+      const attributes = {
+        taskType: definitiveRecordingSid ? "voicemail" : "callback",
+        name:
+          (definitiveRecordingSid ? "Voicemail" : "Callback") +
+          ` (${numberToCall})`,
+        flow_execution_sid: flexFlowSid,
+        message: message || null,
+        callBackData: {
+          numberToCall,
+          numberToCallFrom,
+          attempts,
+          mainTimeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+          utcDateTimeReceived: utcDateTimeReceived || new Date(),
+          recordingSid: definitiveRecordingSid,
+          recordingUrl: definitiveRecordingUrl,
+          transcriptSid: definitiveTranscriptionSid,
+          transcriptText: definitiveTranscriptionText,
+          isDeleted: isDeleted || false,
+        },
+        direction: "inbound",
+        conversations: {
+          conversation_id,
+        },
+      };
+
+      const result = await TaskOperations.createTask({
+        context,
+        workflowSid,
+        taskChannel,
+        attributes,
+        priority,
+        timeout,
+        attempts: 0,
+      });
+      response.setStatusCode(result.status);
+      response.setBody({ success: result.success, taskSid: result.taskSid });
+      callback(null, response);
+    } catch (error) {
+      handleError(error);
+    }
   }
-});
+);
