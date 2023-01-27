1b42126f19bcf88993f806e0526275bdef58d559
Lindsay Hamoudi <lhamoudi@twilio.com>
2023-01-26 16:17:24 -0800

    Changing the interface of create-callback, to better align with transcription callback event param names. And adding transcription callback handler for kicking off the voicemail task.

---
+++ serverless-functions/src/functions/features/callbacks/studio/voicemail-transcription-callback.protected.js
@@ -0,0 +1,9 @@
+// Handles transcription completed callback from Twilio Studio, and creates the voicemail task
+exports.handler = async function (context, event, callback) {
+  const client = context.getTwilioClient();
+
+  const path =
+    Runtime.getFunctions()["features/callbacks/studio/create-callback"].path;
+  const createCallbackOrVoicemail = require(path);
+  return createCallbackOrVoicemail.handler(context, event, callback);
+};
