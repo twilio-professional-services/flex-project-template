fb9ca49692f4d0cea45eda79e6e57cfb93c24701
Lindsay Hamoudi <lhamoudi@twilio.com>
2023-01-26 16:41:13 -0800

    Testing github actions by committing something I can merge!

--- serverless-functions/src/functions/features/callbacks/studio/voicemail-transcription-callback.protected.js
+++ serverless-functions/src/functions/features/callbacks/studio/voicemail-transcription-callback.protected.js
@@ -2,9 +2,14 @@
 exports.handler = async function (context, event, callback) {
   const client = context.getTwilioClient();
 
-  console.log("In transcription completed callback");
   const path =
     Runtime.getFunctions()["features/callbacks/studio/create-callback"].path;
   const createCallbackOrVoicemail = require(path);
+
+  event.recordingSid = event.RecordingSid;
+  event.recordingUrl = event.RecordingUrl;
+  event.transcribeText = event.TranscriptionText;
+  event.transcribeUrl = event.TranscriptionUrl;
+
   return createCallbackOrVoicemail.handler(context, event, callback);
 };
