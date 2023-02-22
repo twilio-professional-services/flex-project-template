4f138dadc7f9b5fd7ffca649afcbb6180abf7ea2
Jared Hunter <jhunter@twilio.com>
2023-02-22 11:39:32 -0600

    correcting rename-feature script to account for recently added flex-hooks

--- scripts/remove-features.js
+++ scripts/remove-features.js
@@ -58,12 +58,20 @@ const filesToCopy = [
     dst: `${pluginSrc}/flex-hooks/jsclient-event-listeners/`,
   },
   {
+    src: `${templateDirectory}/no-features/flex-hooks/jsclient-event-listeners/conversations-client/*`,
+    dst: `${pluginSrc}/flex-hooks/jsclient-event-listeners/conversations-client/`,
+  },
+  {
     src: `${templateDirectory}/no-features/flex-hooks/jsclient-event-listeners/voice-client/*`,
     dst: `${pluginSrc}/flex-hooks/jsclient-event-listeners/voice-client/`,
   },
   {
     src: `${templateDirectory}/no-features/flex-hooks/jsclient-event-listeners/worker-client/*`,
     dst: `${pluginSrc}/flex-hooks/jsclient-event-listeners/worker-client/`,
+  },
+  {
+    src: `${templateDirectory}/no-features/flex-hooks/chat-orchestrator.ts`,
+    dst: `${pluginSrc}/flex-hooks/chat-orchestrator/index.ts`,
   },
   {
     src: `${templateDirectory}/no-features/types/CustomServiceConfiguration.ts`,
