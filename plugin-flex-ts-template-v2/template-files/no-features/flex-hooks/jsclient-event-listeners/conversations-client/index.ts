173a240175c44fe94c37dc94921e9c2b38617c07
Sam Johnson <sam@scj.me>
2023-03-15 23:29:09 +0000

    Remove orphaned script files

--- plugin-flex-ts-template-v2/template-files/no-features/flex-hooks/jsclient-event-listeners/conversations-client/index.ts
+++
@@ -1,6 +0,0 @@
-import * as Flex from "@twilio/flex-ui";
-import conversationJoined from "./conversationJoined";
-
-export default (flex: typeof Flex, manager: Flex.Manager) => {
-  conversationJoined(flex, manager);
-};
