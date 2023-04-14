919c0c63825b67c16ef51800013c7a3bb9bd5621
Jared Hunter <jhunter@twilio.com>
2023-04-14 11:32:31 -0500

    adding developer comment to error code 21220

--- serverless-functions/src/functions/features/dual-channel-recording/flex/create-recording.js
+++ serverless-functions/src/functions/features/dual-channel-recording/flex/create-recording.js
@@ -19,9 +19,22 @@ exports.handler = prepareFlexFunction(requiredParameters, async (context, event,
     });
 
     const { recording, status } = result;
+    let developerComment;
+
+    if (result.twilioErrorCode) {
+      // eslint-disable-next-line sonarjs/no-small-switch
+      switch (result.twilioErrorCode) {
+        case 21220:
+          developerComment =
+            'It is typical to see this error when trying to start a call recording on a call thats already ended.  This can happen when a hungup occurs after the agent accepts the task but before the recording is able to start';
+          break;
+        default:
+          break;
+      }
+    }
 
     response.setStatusCode(status);
-    response.setBody({ recording, ...extractStandardResponse(result) });
+    response.setBody({ recording, ...extractStandardResponse(result), developerComment });
     return callback(null, response);
   } catch (error) {
     return handleError(error);
