1ce853dd3d8f3cc53ff3ba7758e202a31d669e02
Sam Johnson <sam@scj.me>
2023-05-01 14:46:45 -0500

    Fix hidden cold xfer button if warm xfer disabled

--- plugin-flex-ts-template-v2/src/feature-library/conversation-transfer/flex-hooks/actions/ShowDirectory.ts
+++ plugin-flex-ts-template-v2/src/feature-library/conversation-transfer/flex-hooks/actions/ShowDirectory.ts
@@ -8,6 +8,10 @@ export const actionName = FlexAction.ShowDirectory;
 export const actionHook = function handleChatTransferShowDirectory(flex: typeof Flex, manager: Flex.Manager) {
   if (isMultiParticipantEnabled()) return;
 
+  // If warm transfers are disabled within Flex, the button is already removed.
+  // Warm transfers are enabled by default now, so check for the explicit disabled value.
+  if (!manager.store.getState().flex.featureFlags.features['flex-warm-transfers']?.enabled) return;
+
   flex.Actions.addListener(`${actionEvent}${actionName}`, (_payload: any, _abortFunction: any) => {
     let display = 'flex';
     const taskSid = manager.store.getState().flex.view.selectedTaskSid;
