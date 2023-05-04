f7418dcd6d99096376952c7e2e27b1a51a551f49
Sam Johnson <sam@scj.me>
2023-05-04 09:54:27 -0500

    Minor optimization

--- plugin-flex-ts-template-v2/src/feature-library/conference/config.ts
+++ plugin-flex-ts-template-v2/src/feature-library/conference/config.ts
@@ -6,13 +6,14 @@ import ConferenceConfig from './types/ServiceConfiguration';
 const { enabled = false, hold_workaround = false } =
   (getFeatureFlags()?.features?.conference as ConferenceConfig) || {};
 
+const nativeXwtEnabled =
+  Flex.Manager.getInstance().store.getState().flex.featureFlags.features['external-warm-transfers']?.enabled === true;
+
 export const isFeatureEnabled = () => {
   return enabled;
 };
 
 export const isAddButtonEnabled = () => {
-  const nativeXwtEnabled =
-    Flex.Manager.getInstance().store.getState().flex.featureFlags.features['external-warm-transfers']?.enabled === true;
   return enabled && !nativeXwtEnabled;
 };
 
