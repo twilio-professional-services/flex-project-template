b6ea607dce170984ae3a85931a539cb7f93300d8
Sam Johnson <7373633+dremin@users.noreply.github.com>
2023-05-04 13:20:17 -0500

    Auto detect native external warm transfer within conference feature (#211)

--- plugin-flex-ts-template-v2/src/feature-library/conference/config.ts
+++ plugin-flex-ts-template-v2/src/feature-library/conference/config.ts
@@ -1,18 +1,20 @@
+import * as Flex from '@twilio/flex-ui';
+
 import { getFeatureFlags } from '../../utils/configuration';
 import ConferenceConfig from './types/ServiceConfiguration';
 
-const {
-  enabled = false,
-  add_button = false,
-  hold_workaround = false,
-} = (getFeatureFlags()?.features?.conference as ConferenceConfig) || {};
+const { enabled = false, hold_workaround = false } =
+  (getFeatureFlags()?.features?.conference as ConferenceConfig) || {};
+
+const nativeXwtEnabled =
+  Flex.Manager.getInstance().store.getState().flex.featureFlags.features['external-warm-transfers']?.enabled === true;
 
 export const isFeatureEnabled = () => {
   return enabled;
 };
 
 export const isAddButtonEnabled = () => {
-  return enabled && add_button;
+  return enabled && !nativeXwtEnabled;
 };
 
 export const isHoldWorkaroundEnabled = () => {
