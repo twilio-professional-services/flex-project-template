0f368451f727189e42c95bbe996307143291b6a9
Jared Hunter <jhunter@twilio.com>
2023-05-08 15:42:49 -0500

    renaming folder

--- plugin-flex-ts-template-v2/src/feature-library/hang-up-by/flex-hooks/events/pluginsInitialized.ts
+++ plugin-flex-ts-template-v2/src/feature-library/hang-up-by/flex-hooks/events/pluginsInitialized.ts
@@ -1,6 +1,6 @@
 import { resetHangUpBy } from '../../helpers/hangUpBy';
 import { FlexEvent } from '../../../../types/feature-loader';
-import { registerSetHangUpByAction } from '../custom-action/setHangUpBy';
+import { registerSetHangUpByAction } from '../custom-actions/setHangUpBy';
 
 export const eventName = FlexEvent.pluginsInitialized;
 export const eventHook = () => {
