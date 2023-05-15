d1faa8f0ed2daf27c43932c6a0c56e3f61ed6b8b
Sam Johnson <7373633+dremin@users.noreply.github.com>
2023-05-15 11:50:59 -0500

    Update conversation-transfer functions to reduce boilerplate (#224)

--- serverless-functions/src/functions/features/conversation-transfer/flex/remove-participant.js
+++ serverless-functions/src/functions/features/conversation-transfer/flex/remove-participant.js
@@ -1,48 +1,23 @@
-const TokenValidator = require('twilio-flex-token-validator').functionValidator;
-
-const FunctionHelper = require(Runtime.getFunctions()['common/helpers/function-helper'].path);
+const { prepareFlexFunction } = require(Runtime.getFunctions()['common/helpers/function-helper'].path);
 const ConversationsOperations = require(Runtime.getFunctions()['common/twilio-wrappers/conversations'].path);
 const InteractionsOperations = require(Runtime.getFunctions()['common/twilio-wrappers/interactions'].path);
 
-const getRequiredParameters = () => {
-  return [
-    {
-      key: 'flexInteractionSid',
-      purpose: 'KDxxx sid for inteactions API',
-    },
-    {
-      key: 'flexInteractionChannelSid',
-      purpose: 'UOxxx sid for interactions API',
-    },
-    {
-      key: 'flexInteractionParticipantSid',
-      purpose: 'UTxxx sid for interactions API',
-    },
-  ];
-};
+const requiredParameters = [
+  {
+    key: 'flexInteractionSid',
+    purpose: 'KDxxx sid for inteactions API',
+  },
+  {
+    key: 'flexInteractionChannelSid',
+    purpose: 'UOxxx sid for interactions API',
+  },
+  {
+    key: 'flexInteractionParticipantSid',
+    purpose: 'UTxxx sid for interactions API',
+  },
+];
 
-exports.handler = TokenValidator(async function chat_transfer_v2_cbm(context, event, callback) {
-  const response = new Twilio.Response();
-
-  const requiredParameters = getRequiredParameters();
-  const parameterError = FunctionHelper.validateParameters(context.PATH, event, requiredParameters);
-
-  response.appendHeader('Access-Control-Allow-Origin', '*');
-  response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS POST');
-  response.appendHeader('Content-Type', 'application/json');
-  response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (Object.keys(event).length === 0) {
-    console.log('Empty event object, likely an OPTIONS request');
-    return callback(null, response);
-  }
-
-  if (parameterError) {
-    response.setStatusCode(400);
-    response.setBody({ data: null, message: parameterError });
-    return callback(null, response);
-  }
-
+exports.handler = prepareFlexFunction(requiredParameters, async (context, event, callback, response, handleError) => {
   try {
     const { flexInteractionSid, flexInteractionChannelSid, flexInteractionParticipantSid, conversationSid } = event;
 
@@ -81,9 +56,6 @@ exports.handler = TokenValidator(async function chat_transfer_v2_cbm(context, ev
     });
     return callback(null, response);
   } catch (error) {
-    console.error(`Unexpected error occurred in ${scriptName}: ${error}`);
-    response.setStatusCode(500);
-    response.setBody({ success: false, message: error });
-    return callback(null, response);
+    return handleError(error);
   }
 });
