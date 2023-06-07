7c824da42753059652dc09ec8735da015c6708ca
Sam Johnson <sam@scj.me>
2023-06-07 11:32:32 -0500

    Make remaining features localizable

--- plugin-flex-ts-template-v2/src/feature-library/teams-view-filters/filters/departmentFilter.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/teams-view-filters/filters/departmentFilter.tsx
@@ -1,11 +1,12 @@
-import { FilterDefinition } from '@twilio/flex-ui';
+import { FilterDefinition, Manager } from '@twilio/flex-ui';
 
 import SelectFilter from '../custom-components/SelectFilter';
 import SelectFilterLabel from '../custom-components/SelectFilterLabel';
 import { getDepartmentOptions } from '../config';
+import { StringTemplates } from '../flex-hooks/strings/TeamViewQueueFilter';
 
 /* 
-  This filter is based on the model of the worker attibutes adopted from
+  This filter is based on the model of the worker attributes adopted from
   flex insights.  For a definition of that model see:
 
   https://www.twilio.com/docs/flex/developer/insights/enhance-integration#enhance-agent-data
@@ -21,7 +22,7 @@ import { getDepartmentOptions } from '../config';
   but there is no consistent way to do this without a backend solution.
 
   If building tooling in the solution to allow supervisors the ability
-  to edit and apply department name, synconrizing these options should be 
+  to edit and apply department name, synchronizing these options should be 
   a consideration.
 
   */
@@ -29,7 +30,7 @@ import { getDepartmentOptions } from '../config';
 export const departmentFilter = () =>
   ({
     id: 'data.attributes.department_name',
-    title: 'Departments',
+    title: (Manager.getInstance().strings as any)[StringTemplates.Department],
     fieldName: 'department_name',
     options: getDepartmentOptions()
       .sort()
--- plugin-flex-ts-template-v2/src/feature-library/teams-view-filters/flex-hooks/strings/TeamViewQueueFilter.ts
+++ plugin-flex-ts-template-v2/src/feature-library/teams-view-filters/flex-hooks/strings/TeamViewQueueFilter.ts
@@ -3,6 +3,16 @@ export enum StringTemplates {
   ErrorParsingQueueExpression = 'QueueFilterParseNotification',
   ErrorParsingQueueExpressionWithOR = 'QueueFilterParseORsNotification',
   ErrorQueueNotFound = 'QueueFilterQueueNotFound',
+  FilterOnly = 'PSTeamsViewFilterOnly',
+  FilterContaining = 'PSTeamsViewFilterContaining',
+  SelectItem = 'PSTeamsViewSelectItem',
+  MultiSelectItems = 'PSTeamsViewMultiSelectItems',
+  MultiSelectedItems = 'PSTeamsViewMultiSelectedItems',
+  AgentSkills = 'PSTeamsViewAgentSkills',
+  Department = 'PSTeamsViewDepartment',
+  EmailAddress = 'PSTeamsViewEmailAddress',
+  QueueEligibility = 'PSTeamsViewQueueEligibility',
+  Team = 'PSTeamsViewTeam',
 }
 
 export const stringHook = () => ({
@@ -11,5 +21,15 @@ export const stringHook = () => ({
     [StringTemplates.ErrorParsingQueueExpressionWithOR]:
       "Unable to apply queue filters to queues containing OR'd expressions. Ignoring queue filter.",
     [StringTemplates.ErrorQueueNotFound]: 'Queue reference not found, ignoring queue filter',
+    [StringTemplates.FilterOnly]: '{{selected}} only',
+    [StringTemplates.FilterContaining]: 'Containing "{{selected}}"',
+    [StringTemplates.SelectItem]: 'Select an item...',
+    [StringTemplates.MultiSelectItems]: 'Select one or more items...',
+    [StringTemplates.MultiSelectedItems]: 'Selected items:',
+    [StringTemplates.AgentSkills]: 'Agent Skills',
+    [StringTemplates.Department]: 'Department',
+    [StringTemplates.EmailAddress]: 'Email Address',
+    [StringTemplates.QueueEligibility]: 'Queue Eligibility',
+    [StringTemplates.Team]: 'Team',
   },
 });
