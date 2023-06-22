1553d4a2faa0ab8fdcb8f596ce777083312a0dd4
Jared Hunter <jhunter@twilio.com>
2023-06-22 18:33:03 -0500

    testsing max_reserved_workers

--- infra-as-code/terraform/modules/routing/task_queues.tf
+++ infra-as-code/terraform/modules/routing/task_queues.tf
@@ -1,5 +1,7 @@
 resource "twilio_taskrouter_workspaces_task_queues_v1" "everyone" {
   workspace_sid  = twilio_taskrouter_workspaces_v1.flex.sid
   friendly_name  = "Everyone"
-  target_workers = "1=0"
+  target_workers = "1==1"
+  max_reserved_workers = 1
+  
 }
