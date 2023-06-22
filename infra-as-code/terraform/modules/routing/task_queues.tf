459b5f273b5fd38ccb75964d9aba4e02e374c5de
Jared Hunter <jhunter@twilio.com>
2023-06-22 18:36:05 -0500

    testing task_order

--- infra-as-code/terraform/modules/routing/task_queues.tf
+++ infra-as-code/terraform/modules/routing/task_queues.tf
@@ -2,6 +2,6 @@ resource "twilio_taskrouter_workspaces_task_queues_v1" "everyone" {
   workspace_sid  = twilio_taskrouter_workspaces_v1.flex.sid
   friendly_name  = "Everyone"
   target_workers = "1==1"
-  max_reserved_workers = 1
-  
+  max_reserved_workers = 2
+  task_order = "FIFO"
 }
