94b4e4dcac97c50b0ee0c5591462a6b93a33a32d
Jared Hunter <jhunter@twilio.com>
2023-06-22 18:48:43 -0500

    testing creating queue that already exists but not in state

--- infra-as-code/terraform/modules/routing/task_queues.tf
+++ infra-as-code/terraform/modules/routing/task_queues.tf
@@ -2,7 +2,7 @@ resource "twilio_taskrouter_workspaces_task_queues_v1" "everyone" {
   workspace_sid  = twilio_taskrouter_workspaces_v1.flex.sid
   friendly_name  = "Everyone"
   target_workers = "1==1"
-  max_reserved_workers = 2
+  max_reserved_workers = 1
   task_order = "FIFO"
 }
 
@@ -10,6 +10,14 @@ resource "twilio_taskrouter_workspaces_task_queues_v1" "sales" {
   workspace_sid  = twilio_taskrouter_workspaces_v1.flex.sid
   friendly_name  = "Sales"
   target_workers = "routing.skills HAS 'sales'"
+  max_reserved_workers = 1
+  task_order = "FIFO"
+}
+
+resource "twilio_taskrouter_workspaces_task_queues_v1" "support" {
+  workspace_sid  = twilio_taskrouter_workspaces_v1.flex.sid
+  friendly_name  = "Support"
+  target_workers = "routing.skills HAS 'support'"
   max_reserved_workers = 1
   task_order = "FIFO"
 }
