1f89d16332555a695546915597d2c20063fff337
Jared Hunter <jhunter@twilio.com>
2023-06-22 18:31:12 -0500

    testing target_workers expression

--- infra-as-code/terraform/modules/routing/task_queues.tf
+++ infra-as-code/terraform/modules/routing/task_queues.tf
@@ -1,4 +1,5 @@
 resource "twilio_taskrouter_workspaces_task_queues_v1" "everyone" {
   workspace_sid  = twilio_taskrouter_workspaces_v1.flex.sid
   friendly_name  = "Everyone"
+  target_workers = "1=0"
 }
