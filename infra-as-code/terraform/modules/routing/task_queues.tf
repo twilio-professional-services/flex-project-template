d694ffc826a7dd3324d949b28a548af70ca136b6
Jared Hunter <jhunter@twilio.com>
2023-06-22 18:45:28 -0500

    testing adding sales queue

--- infra-as-code/terraform/modules/routing/task_queues.tf
+++ infra-as-code/terraform/modules/routing/task_queues.tf
@@ -5,3 +5,11 @@ resource "twilio_taskrouter_workspaces_task_queues_v1" "everyone" {
   max_reserved_workers = 2
   task_order = "FIFO"
 }
+
+resource "twilio_taskrouter_workspaces_task_queues_v1" "sales" {
+  workspace_sid  = twilio_taskrouter_workspaces_v1.flex.sid
+  friendly_name  = "Sales"
+  target_workers = "routing.skills HAS 'sales'"
+  max_reserved_workers = 1
+  task_order = "FIFO"
+}
