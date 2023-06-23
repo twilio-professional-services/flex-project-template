dbe1885d7def4629d5a424430e7387133b53b53c
Jared Hunter <jhunter@twilio.com>
2023-06-23 16:43:21 -0500

    adding callback workflow

--- infra-as-code/terraform/modules/ivr/variables.tf
+++ infra-as-code/terraform/modules/ivr/variables.tf
@@ -3,6 +3,16 @@ variable "workflow_sid_assign_to_anyone" {
   description = "SID of the Assign To Anyone workflow"
 }
 
+variable "workflow_sid_chat_transfer" {
+  type        = string
+  description = "SID of the Chat Transfer workflow"
+}
+
+variable "workflow_sid_callback" {
+  type        = string
+  description = "SID of the Callback workflow"
+}
+
 variable "chat_channel_sid" {
   type        = string
   description = "SID of the chat task channel"
