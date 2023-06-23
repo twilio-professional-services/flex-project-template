0c76a542e1464289a274d1adbf69e3ff1cdf7335
Sam Johnson <sam@scj.me>
2023-06-23 10:57:31 -0500

    Add cascading menus to canned responses

--- plugin-flex-ts-template-v2/src/feature-library/canned-responses/flex-hooks/paste-elements/index.ts
+++ plugin-flex-ts-template-v2/src/feature-library/canned-responses/flex-hooks/paste-elements/index.ts
@@ -3,7 +3,6 @@ import { PasteCustomCSS } from '@twilio-paste/customization';
 export const pasteElementHook = {
   CANNED_RESPONSES_MENU: {
     maxHeight: '70vh',
-    height: '100%',
     overflowY: 'scroll',
   },
   CANNED_RESPONSES_MENU_BUTTON: {
