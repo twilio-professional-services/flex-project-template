b73e671b74c0d541dab27d155aa2803c308c20f2
Jared Hunter <jhunter@twilio.com>
2023-06-23 14:27:28 -0500

    test

--- infra-as-code/terraform/modules/routing/workflows.tf
+++ infra-as-code/terraform/modules/routing/workflows.tf
@@ -1,11 +1,11 @@
 resource "twilio_taskrouter_workspaces_workflows_v1" "default" {
   workspace_sid = twilio_taskrouter_workspaces_v1.flex.sid
   friendly_name = "Assign to Anyone"
-  configuration = templatefile("../../taskrouter/workflow.json", local.params)
+  configuration = templatefile("../../taskrouter/assign_to_anyone.json", local.params)
 }
 
 locals{
   params = {
-    "QUEUE_SID" = twilio_taskrouter_workspaces_task_queues_v1.everyone.sid
+    "QUEUE_SID_EVERYONE" = twilio_taskrouter_workspaces_task_queues_v1.everyone.sid
   }
 }
