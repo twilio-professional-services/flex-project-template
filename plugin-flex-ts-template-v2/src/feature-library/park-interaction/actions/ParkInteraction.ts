3da99a924c26866ac69b6211d802a24ac4962182
Sam Johnson <sam@scj.me>
2023-06-27 10:38:03 -0500

    Work around Flex bug with parking

--- plugin-flex-ts-template-v2/src/feature-library/park-interaction/actions/ParkInteraction.ts
+++ plugin-flex-ts-template-v2/src/feature-library/park-interaction/actions/ParkInteraction.ts
@@ -1,4 +1,4 @@
-import { TaskHelper, Notifications, templates } from '@twilio/flex-ui';
+import { TaskHelper, Notifications, templates, Manager } from '@twilio/flex-ui';
 
 import { ParkInteractionNotification } from '../flex-hooks/notifications';
 import { StringTemplates } from '../flex-hooks/strings';
@@ -43,6 +43,19 @@ export const parkInteraction = async (payload: ParkInteractionPayload) => {
     );
 
     Notifications.dismissNotificationById(ParkInteractionNotification.ParkError);
+
+    setTimeout(() => {
+      console.log('after 2 sec');
+      Manager.getInstance().store.dispatch({
+        type: 'CONVERSATION_UNLOAD',
+        payload: {},
+        meta: {
+          conversationSid: agent.mediaProperties.conversationSid,
+          channelSid: agent.mediaProperties.conversationSid,
+        },
+      });
+    }, 2000);
+
     return Notifications.showNotification(ParkInteractionNotification.ParkSuccess);
   } catch (error) {
     let message = (error as any)?.message;
