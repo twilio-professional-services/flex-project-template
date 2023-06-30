74c7ae5c56e692f5a6ecafe04a6c14ed4d3b4185
Sam Johnson <sam@scj.me>
2023-06-30 12:46:23 -0500

    Fix freeze from dual-channel feature during accept

--- plugin-flex-ts-template-v2/src/feature-library/dual-channel-recording/flex-hooks/events/taskAccepted.ts
+++ plugin-flex-ts-template-v2/src/feature-library/dual-channel-recording/flex-hooks/events/taskAccepted.ts
@@ -1,7 +1,6 @@
 import * as Flex from '@twilio/flex-ui';
 
-import { addCallDataToTask, waitForConferenceParticipants, waitForActiveCall } from '../../helpers/dualChannelHelper';
-import DualChannelService from '../../helpers/DualChannelService';
+import { recordExternalCall, recordInternalCall } from '../../helpers/dualChannelHelper';
 import { getChannelToRecord } from '../../config';
 import { FlexEvent } from '../../../../types/feature-loader';
 
@@ -13,7 +12,6 @@ export const eventHook = async (flex: typeof Flex, _manager: Flex.Manager, task:
 
   const { attributes } = task;
   const { client_call, direction, conversations } = attributes;
-  let callSid;
 
   if (conversations && conversations.media && getChannelToRecord() === 'customer') {
     // This indicates a recording has already been started for this call
@@ -24,51 +22,14 @@ export const eventHook = async (flex: typeof Flex, _manager: Flex.Manager, task:
   if (client_call && direction === 'outbound') {
     // internal call - always record based on call SID, as conference state is unknown by Flex
     // Record only the outbound leg to prevent duplicate recordings
-    console.debug('Waiting for internal call to begin');
-    callSid = await waitForActiveCall(task);
-    console.debug('Recorded internal call:', callSid);
+    // Do not await so that event processing is not blocked
+    recordInternalCall(task);
   } else if (client_call) {
     // internal call, inbound leg - skip recording this leg
     console.debug('Skipping recording for inbound internal call', task.sid);
   } else {
-    // We want to wait for all participants (customer and worker) to join the
-    // conference before we start the recording
-    console.debug('Waiting for customer and worker to join the conference');
-    const participants = await waitForConferenceParticipants(task);
-
-    let participantLeg;
-    switch (getChannelToRecord()) {
-      case 'customer': {
-        participantLeg = participants.find((p) => p.participantType === 'customer');
-        break;
-      }
-      case 'worker': {
-        participantLeg = participants.find((p) => p.participantType === 'worker' && p.isCurrentWorker);
-        break;
-      }
-      default:
-        break;
-    }
-
-    console.debug('Recorded Participant: ', participantLeg);
-
-    if (!participantLeg) {
-      console.warn('No customer or worker participant. Not starting the call recording');
-      return;
-    }
-
-    ({ callSid } = participantLeg);
-  }
-
-  if (!callSid) {
-    console.warn('Unable to determine call SID for recording');
-    return;
-  }
-
-  try {
-    const recording = await DualChannelService.startDualChannelRecording(callSid);
-    await addCallDataToTask(task, callSid, recording);
-  } catch (error) {
-    console.error('Unable to start dual channel recording.', error);
+    // External call
+    // Do not await so that event processing is not blocked
+    recordExternalCall(task);
   }
 };
