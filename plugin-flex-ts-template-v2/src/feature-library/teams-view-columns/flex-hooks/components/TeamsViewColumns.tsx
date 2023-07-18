9e821c1ca90df6967414d901cf73bc9dd11bb113
Ludo Vlieger <lvlieger@twilio.com>
2023-07-18 09:10:07 -0400

    Clean up

--- plugin-flex-ts-template-v2/src/feature-library/teams-view-columns/flex-hooks/components/TeamsViewColumns.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/teams-view-columns/flex-hooks/components/TeamsViewColumns.tsx
@@ -5,28 +5,31 @@ import { Worker } from '../../../../types/task-router';
 import { FlexComponent } from '../../../../types/feature-loader';
 
 interface WorkerItem {
-  worker: Worker
+  worker: Worker;
 }
 
 const getSkills = (item: WorkerItem) => {
-  return item.worker.attributes.routing ?
-    item.worker?.attributes?.routing?.skills?.join(' / ') : 'NONE'
-}
+  return item.worker.attributes.routing ? item.worker?.attributes?.routing?.skills?.join(' / ') : 'NONE';
+};
 export const componentName = FlexComponent.TaskCanvasHeader;
 export const componentHook = function addWorkersDataTableColumns(flex: typeof Flex) {
+  flex.WorkersDataTable.Content.add(
+    <flex.ColumnDefinition
+      key="team"
+      header={'Team'}
+      style={{ width: 150 }}
+      content={(item: WorkerItem) => item.worker.attributes.team_name}
+    />,
+    { sortOrder: 4 }
+  );
 
-  flex.WorkersDataTable.Content.add(<flex.ColumnDefinition
-    key="team"
-    header={"Team"}
-    style={{ width: 150 }}
-    content={(item: WorkerItem) => item.worker.attributes.team_name}
-  />, { sortOrder: 4 });
-
-  flex.WorkersDataTable.Content.add(<flex.ColumnDefinition
-    key="skills"
-    header={"Skills"}
-    style={{ width: 200 }}
-    content={(item: WorkerItem) => getSkills(item)}
-  />, { sortOrder: 5 });
-
+  flex.WorkersDataTable.Content.add(
+    <flex.ColumnDefinition
+      key="skills"
+      header={'Skills'}
+      style={{ width: 200 }}
+      content={(item: WorkerItem) => getSkills(item)}
+    />,
+    { sortOrder: 5 }
+  );
 };
