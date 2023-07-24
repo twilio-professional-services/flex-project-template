b5334577dbfb1f64e60c14cbc360354e8a8d711f
Jared Hunter <jhunter@twilio.com>
2023-07-23 23:07:05 -0500

    updating comment and variable names

--- plugin-flex-ts-template-v2/src/feature-library/activity-reservation-handler/helper/ActivityManager.ts
+++ plugin-flex-ts-template-v2/src/feature-library/activity-reservation-handler/helper/ActivityManager.ts
@@ -77,7 +77,7 @@ interface PendingActivity {
 interface FunctionPromise {
   resolve: any;
   reject: any;
-  overrideCurrentState: boolean;
+  overrideAvailability: boolean;
 }
 
 class ActivityManager {
@@ -96,6 +96,7 @@ class ActivityManager {
     this.updateState();
   }
 
+  // expose method to cache an activity to change to
   storePendingActivityChange = (activity: string) => {
     // Pulling out only the relevant activity properties to avoid
     // a circular structure error in JSON.stringify()
@@ -106,17 +107,26 @@ class ActivityManager {
     localStorage.setItem(this.pendingActivityChangeItemKey, JSON.stringify(pendingActivityChange));
   };
 
-  updateState = async (overrideCurrentState?: boolean) => {
+  // externally exposed method to evaluate which state
+  // we should be in and whether we should update it.
+  // overrideAvailability is passed only when we want to
+  // change the availability we are currently in. For example
+  // when in a reserved system state but want to go unavailable
+  // this method uses a semaphore to enforce a single execution
+  // at a time.
+  updateState = async (overrideAvailability?: boolean) => {
     return new Promise(async (resolve, reject) => {
       this.currentRequests.push({
         resolve,
         reject,
-        overrideCurrentState,
+        overrideAvailability,
       } as FunctionPromise);
       await this.#tryNext();
     });
   };
 
+  // externally exposed method to change the worker state
+  // was only exposed for use in StartOutboundCall
   setWorkerActivity = async (activityName: string) => {
     if (activityName === this.getPendingActivity()?.name) this.#clearPendingActivity();
     Actions.invokeAction('SetActivity', {
@@ -127,6 +137,8 @@ class ActivityManager {
     await this.#waitForWorkerActivityChange(activityName);
   };
 
+  // externally exposed method to provide the cached activity
+  // was only exposed for PendingActivityComponent
   getPendingActivity = (): PendingActivity => {
     const item = localStorage.getItem(this.pendingActivityChangeItemKey);
     const pendingActivity: PendingActivity = item && JSON.parse(item);
@@ -136,9 +148,9 @@ class ActivityManager {
   #tryNext = async () => {
     if (!this.currentRequests.length) {
     } else if (this.runningRequests < this.maxConcurrentRequests) {
-      const { resolve, reject, overrideCurrentState } = this.currentRequests.shift() as FunctionPromise;
+      const { resolve, reject, overrideAvailability } = this.currentRequests.shift() as FunctionPromise;
       this.runningRequests += 1;
-      const req = this.#updateState(overrideCurrentState);
+      const req = this.#updateState(overrideAvailability);
       req
         .then((res) => resolve(res))
         .catch((err) => reject(err))
@@ -149,15 +161,16 @@ class ActivityManager {
     }
   };
 
-  #updateState = async (overrideCurrentState?: boolean) => {
+  // performs the algorithm to evaluate whether we should switch state
+  #updateState = async (overrideAvailability?: boolean) => {
     const { available } = systemActivityNames;
 
     const currentWorkerActivity = getCurrentWorkerActivity();
 
     // when evaluating the next state, we need to know whether we want to be on or off acd
-    // overrideCurrentstate is used when manually selecting the activity from the agent drop down
+    // overrideAvailability is used when manually selecting the activity from the agent drop down
     const acdAvailabilityStatus =
-      overrideCurrentState === undefined ? currentWorkerActivity?.available || false : overrideCurrentState;
+      overrideAvailability === undefined ? currentWorkerActivity?.available || false : overrideAvailability;
 
     // evaluate what the new activity/state should be
     const newActivity = this.#evaluateNewState(acdAvailabilityStatus);
@@ -172,6 +185,7 @@ class ActivityManager {
     if (newActivity !== currentWorkerActivity?.name) await this.setWorkerActivity(newActivity);
   };
 
+  // evaluates which state we should be in given an availability status
   #evaluateNewState = (newAvailabilityStatus: boolean): string => {
     const { onATask, onATaskNoAcd, wrapup, wrapupNoAcd } = systemActivityNames;
     const selectedTaskStatus = getSelectedTaskStatus();
