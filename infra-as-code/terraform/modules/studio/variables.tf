e998cb4caa32ebbb2adfc0245703aa94b7ab5c88
Jared Hunter <jhunter@twilio.com>
2023-07-25 21:02:41 -0500

    adding in configuration for function service sids, environment sids and studio function calls to be more readily used in future studio development

--- infra-as-code/terraform/modules/studio/variables.tf
+++ infra-as-code/terraform/modules/studio/variables.tf
@@ -52,20 +52,74 @@ variable "voice_channel_sid" {
   }
 }
 
-variable "domain_custom_flex" {
+variable "serverless_domain" {
   type        = string
   description = "serverless domain for flex plugin"
   validation {
-    condition     = length(var.domain_custom_flex) > 34 && substr(var.domain_custom_flex, 0, 34) == "custom-flex-extensions-serverless-"
-    error_message = "domain_custom_flex expected to start with \"custom-flex-extensions-serverless-\"."
+    condition     = length(var.serverless_domain) > 34 && substr(var.serverless_domain, 0, 34) == "custom-flex-extensions-serverless-"
+    error_message = "serverless_domain expected to start with \"custom-flex-extensions-serverless-\"."
   }
 }
 
-variable "domain_schedule_manager" {
+variable "serverless_sid" {
+  type        = string
+  description = "serverless sid"
+  validation {
+    condition     = length(var.serverless_sid) > 2 && substr(var.serverless_sid, 0, 2) == "ZS"
+    error_message = "serverless_sid expected to start with \"ZS\"."
+  }
+}
+
+variable "serverless_env_sid" {
+  type        = string
+  description = "serverless env sid"
+  validation {
+    condition     = length(var.serverless_env_sid) > 2 && substr(var.serverless_env_sid, 0, 2) == "ZE"
+    error_message = "serverless_env_sid expected to start with \"ZE\"."
+  }
+}
+
+variable "schedule_manager_domain" {
   type        = string
   description = "serverless domain schedule manager for flex plugin"
   validation {
-    condition     = length(var.domain_schedule_manager) > 17 && substr(var.domain_schedule_manager, 0, 17) == "schedule-manager-"
-    error_message = "domain_schedule_manager expected to start with \"schedule-manager-\"."
+    condition     = length(var.schedule_manager_domain) > 17 && substr(var.schedule_manager_domain, 0, 17) == "schedule-manager-"
+    error_message = "schedule_manager_domain expected to start with \"schedule-manager-\"."
+  }
+}
+
+variable "schedule_manager_sid" {
+  type        = string
+  description = "serverless sid"
+  validation {
+    condition     = length(var.schedule_manager_sid) > 2 && substr(var.schedule_manager_sid, 0, 2) == "ZS"
+    error_message = "schedule_manager_sid expected to start with \"ZS\"."
+  }
+}
+
+variable "schedule_manager_env_sid" {
+  type        = string
+  description = "schedule manager env sid"
+  validation {
+    condition     = length(var.schedule_manager_env_sid) > 2 && substr(var.schedule_manager_env_sid, 0, 2) == "ZE"
+    error_message = "schedule_manager_env_sid expected to start with \"ZE\"."
+  }
+}
+
+variable "function_create_callback" {
+  type        = string
+  description = "create callback function sid"
+  validation {
+    condition     = length(var.function_create_callback) > 2 && substr(var.function_create_callback, 0, 2) == "ZH"
+    error_message = "function_create_callback expected to start with \"ZH\"."
+  }
+}
+
+variable "function_check_schedule_sid" {
+  type        = string
+  description = "get customer by phone function sid"
+  validation {
+    condition     = length(var.function_check_schedule_sid) > 2 && substr(var.function_check_schedule_sid, 0, 2) == "ZH"
+    error_message = "function_check_schedule_sid expected to start with \"ZH\"."
   }
 }
