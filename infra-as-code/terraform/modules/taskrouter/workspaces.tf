3b185f17548cb18ffaa10fcc0238781d8e9c8b89
Jared Hunter <jhunter@twilio.com>
2023-08-23 15:07:51 -0500

    consolidating params

--- infra-as-code/terraform/modules/taskrouter/workspaces.tf
+++ infra-as-code/terraform/modules/taskrouter/workspaces.tf
@@ -13,8 +13,13 @@ resource "twilio_taskrouter_workspaces_v1" "flex" {
   events_filter = "task-queue.created,task-queue.expression.updated,worker.attributes.update,worker.created"
 }
 
+
 locals{
   params = {
+    "QUEUE_SID_EVERYONE" = twilio_taskrouter_workspaces_task_queues_v1.everyone.sid
+    "QUEUE_SID_TEMPLATE_EXAMPLE_SALES" = twilio_taskrouter_workspaces_task_queues_v1.template_example_sales.sid
+    "QUEUE_SID_TEMPLATE_EXAMPLE_SUPPORT" = twilio_taskrouter_workspaces_task_queues_v1.template_example_support.sid
+    "QUEUE_SID_INTERNAL_CALLS" = twilio_taskrouter_workspaces_task_queues_v1.internal_calls.sid
     "SERVERLESS_DOMAIN" = var.serverless_domain
   }
 }
