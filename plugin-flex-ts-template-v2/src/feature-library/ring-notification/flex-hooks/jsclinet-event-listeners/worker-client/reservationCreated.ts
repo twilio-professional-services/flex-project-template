e70776018a0eeaad86a0d8b359ab85d986bd30c4
Viral Gandhi <vigandhi@twilio.com>
2023-08-28 10:54:22 +0530

    Fix for lint errors

--- plugin-flex-ts-template-v2/src/feature-library/ring-notification/flex-hooks/jsclinet-event-listeners/worker-client/reservationCreated.ts
+++ plugin-flex-ts-template-v2/src/feature-library/ring-notification/flex-hooks/jsclinet-event-listeners/worker-client/reservationCreated.ts
@@ -4,7 +4,7 @@ import { FlexJsClient, WorkerEvent } from '../../../../../types/feature-loader';
 import { Reservation } from '../../../../../types/task-router';
 import AudioPlayerManagerHelper from '../../../helpers/audioPlayerManagerHelper';
 
-//With this feature we are using jsClientHook instead of manager events since it makes code much simpler.
+// With this feature we are using jsClientHook instead of manager events since it makes code much simpler.
 export const clientName = FlexJsClient.workerClient;
 export const eventName = WorkerEvent.reservationCreated;
 export const jsClientHook = function reservationCreatedHandler(
