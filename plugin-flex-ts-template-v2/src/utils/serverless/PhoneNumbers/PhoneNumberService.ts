b676472e48104c51667fb8240b50c593d0e08461
Sam Johnson <sam@scj.me>
2023-09-18 16:29:31 -0500

    Separate cache with outgoing numbers

--- plugin-flex-ts-template-v2/src/utils/serverless/PhoneNumbers/PhoneNumberService.ts
+++ plugin-flex-ts-template-v2/src/utils/serverless/PhoneNumbers/PhoneNumberService.ts
@@ -41,13 +41,19 @@ class PhoneNumberService extends ApiService {
 
   private STORAGE_KEY_FLEX_PHONE_NUMBERS = `FLEX_PHONE_NUMBERS_${this.flex_service_instance_sid}`;
 
+  private STORAGE_KEY_FLEX_PHONE_NUMBERS_WITH_OUTGOING = `FLEX_PHONE_NUMBERS_WITH_OUTGOING_${this.flex_service_instance_sid}`;
+
   private STORAGE_KEY_VALIDATED_NUMBERS = `FLEX_VALIDATED_NUMBERS`;
 
   private EXPIRY = 86400000; // 1 day
 
   async getAccountPhoneNumbers(includeOutgoing: boolean): Promise<ListPhoneNumbersResponse> {
     // look for value in storage first
-    const cachedResult = JSON.parse(localStorage.getItem(this.STORAGE_KEY_FLEX_PHONE_NUMBERS) || '{}');
+    const cachedResult = JSON.parse(
+      localStorage.getItem(
+        includeOutgoing ? this.STORAGE_KEY_FLEX_PHONE_NUMBERS_WITH_OUTGOING : this.STORAGE_KEY_FLEX_PHONE_NUMBERS,
+      ) || '{}',
+    );
     // if storage value has expired, discard
     if (cachedResult.expiry < new Date().getTime()) cachedResult.success = false;
     // if we have a valid storage value use it, otherwise get from backend.
@@ -79,7 +85,7 @@ class PhoneNumberService extends ApiService {
       // if response from service was successful, store it
       if (response.success)
         localStorage.setItem(
-          this.STORAGE_KEY_FLEX_PHONE_NUMBERS,
+          includeOutgoing ? this.STORAGE_KEY_FLEX_PHONE_NUMBERS_WITH_OUTGOING : this.STORAGE_KEY_FLEX_PHONE_NUMBERS,
           JSON.stringify({
             ...response,
             expiry: new Date().getTime() + this.EXPIRY,
