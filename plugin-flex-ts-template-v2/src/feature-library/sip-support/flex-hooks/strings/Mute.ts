18970f1aefa55b4d9a2c236da87c3d84307f624a
Christopher Connolly <425070+chaosloth@users.noreply.github.com>
2023-09-19 21:45:52 +1000

    Removed unused strings

--- plugin-flex-ts-template-v2/src/feature-library/sip-support/flex-hooks/strings/Mute.ts
+++ plugin-flex-ts-template-v2/src/feature-library/sip-support/flex-hooks/strings/Mute.ts
@@ -1,10 +1,6 @@
 // Export the template names as an enum for better maintainability when accessing them elsewhere
-export enum StringTemplates {
-  MuteParticipant = 'PSMuteParticipant',
-}
+export enum StringTemplates {}
 
 export const stringHook = () => ({
-  'en-US': {
-    [StringTemplates.MuteParticipant]: 'Mute Participant',
-  },
+  'en-US': {},
 });
