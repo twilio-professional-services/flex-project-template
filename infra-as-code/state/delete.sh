16dddfb21da6aef75ca63a49ede4547a58db5694
Leo Valadares <lvaladares@twilio.com>
2023-09-26 16:07:57 -0300

    adding services folder

--- infra-as-code/state/delete.sh
+++ infra-as-code/state/delete.sh
@@ -3,8 +3,8 @@
 set -e
 echo "Trying to Delete Terraform State for $ENVIRONMENT"
 
-tfstate_sid=$(twilio serverless:list services --properties sid,unique_name | awk '$2 == "tfstate" {print $1}')
+#tfstate_sid=$(twilio serverless:list services --properties sid,unique_name | awk '$2 == "tfstate" {print $1}')
 
-echo "TF State SID is $tfstate_sid"
-
-twilio api:serverless:v1:services:remove --sid $tfstate_sid
+#echo "TF State SID is $tfstate_sid"
+terraform destroy -auto-approve
+twilio api:serverless:v1:services:remove --sid tfstate
