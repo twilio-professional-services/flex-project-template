7afe587df4e244cbafe0322919e6ffb9914df217
John Fischelli <john.fischelli@gmail.com>
2023-09-26 18:15:33 -0500

    decorates all logs with worker information

--- plugin-flex-ts-template-v2/src/utils/logger/index.ts
+++ plugin-flex-ts-template-v2/src/utils/logger/index.ts
@@ -1,21 +1,24 @@
 import { createLogger, transports, format } from 'winston';
 
-const alignColorsAndTime = format.combine(
-  format.colorize({
-    all: true,
-  }),
-  format.label({
-    label: '[Flex Project Template]',
-  }),
-  format.printf((info) => `${info.label} ${info.level} : ${info.message}`),
-);
+import FlexHelperSingleton from '../flex-helper';
 
+const worker = FlexHelperSingleton.getCurrentWorker();
 const logger = createLogger({
   level: 'debug',
-  defaultMeta: { service: 'twilio:flex-ui' },
+  defaultMeta: { service: 'twilio:flex-ui', workerName: worker?.name, workerSid: worker?.sid },
   transports: [
     new transports.Console({
-      format: alignColorsAndTime,
+      format: format.combine(
+        format.colorize({
+          all: true,
+        }),
+        format.label({
+          label: '[Flex Project Template]',
+        }),
+        format.printf((info) => {
+          return `${info.label} ${info.level} : ${info.message}`;
+        }),
+      ),
     }),
   ],
 });
