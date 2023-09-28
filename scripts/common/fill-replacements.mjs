7a2520cf68fcea2352e912449921d6414969125e
Sam Johnson <sam@scj.me>
2023-09-28 16:22:42 -0500

    Fill API key and secret from CLI profile config

--- scripts/common/fill-replacements.mjs
+++ scripts/common/fill-replacements.mjs
@@ -113,6 +113,10 @@ const fillAccountVars = (envVars, account) => {
       envVars[key] = account.accountSid;
     } else if (key == 'AUTH_TOKEN' && account.authToken) {
       envVars[key] = account.authToken;
+    } else if (key == 'TWILIO_API_KEY' && account.apiKey) {
+      envVars[key] = account.apiKey;
+    } else if (key == 'TWILIO_API_SECRET' && account.apiSecret) {
+      envVars[key] = account.apiSecret;
     }
   }
   
