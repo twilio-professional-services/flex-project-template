6eed3c272bf79b85cb427a4935b88f209f709fa1
Leo Valadares <lvaladares@twilio.com>
2023-09-28 12:37:55 -0300

    chore: cleaning up

--- services/builder-react/src/components/Cart/CartButton/CartButton.builder.js
+++
@@ -1,9 +0,0 @@
-import { Builder } from "@builder.io/react";
-import { CartButton } from "./CartButton";
-
-Builder.registerComponent(CartButton, {
-  name: "CartButton",
-  models: ["page", "symbol"],
-  image: "https://unpkg.com/css.gg@2.0.0/icons/svg/shopping-cart.svg",
-  description: "Display the shopping cart sidebar wherever you are",
-});
--- services/builder-react/src/viewModel/useSegment.js
+++
@@ -1,4 +0,0 @@
-export const useSegment = () => {
-	if(!window?.analytics) throw Error('segment not defined');
-	return window?.analytics;
-}
