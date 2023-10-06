bfa7ebfcdb66dcf8f4639a8ec27cceb511ee0e92
Sam Johnson <sam@scj.me>
2023-10-06 12:10:57 -0500

    Update activity handler to use logger, small opt

--- plugin-flex-ts-template-v2/src/feature-library/activity-reservation-handler/flex-hooks/events/workerActivityUpdated.ts
+++ plugin-flex-ts-template-v2/src/feature-library/activity-reservation-handler/flex-hooks/events/workerActivityUpdated.ts
@@ -3,10 +3,11 @@ import * as Flex from '@twilio/flex-ui';
 import { FlexEvent } from '../../../../types/feature-loader';
 import ActivityManager from '../../helper/ActivityManager';
 import Activity from '../../../../types/task-router/Activity';
+import logger from '../../../../utils/logger';
 
 export const eventName = FlexEvent.workerActivityUpdated;
 export const eventHook = async (_flex: typeof Flex, _manager: Flex.Manager, activity: Activity) => {
-  console.log(`activity-reservation-handler: handle ${eventName} for ${activity.name}`);
+  logger.debug(`activity-reservation-handler: handle ${eventName} for ${activity.name}`);
 
   // This event is needed to handle activity changes by a supervisor, but it also
   // runs for local changes made by the worker or this feature. When we set activity
