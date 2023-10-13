9f4513eca9c0add7afbee9e867928f49b19180e6
Sam Johnson <sam@scj.me>
2023-10-13 18:02:00 -0500

    Add uninstall script option

--- scripts/common/install-packages.mjs
+++ scripts/common/install-packages.mjs
@@ -1,10 +1,20 @@
 import shell from "shelljs";
 
-export default (packageDir) => {
+export default (packageDir, skipInstall, uninstall) => {
   if (packageDir && shell.test('-d', packageDir)) {
-    console.log(`Installing package ${packageDir}...`);
-    shell.cd(`./${packageDir}`)
-    shell.exec("npm install", {silent:true});
+    shell.cd(`./${packageDir}`);
+    
+    if (uninstall) {
+      console.log(`Uninstalling package ${packageDir}...`);
+      shell.rm('-rf', 'node_modules');
+      shell.rm('-f', 'package-lock.json');
+    }
+    
+    if (!skipInstall) {
+      console.log(`Installing package ${packageDir}...`);
+      shell.exec("npm install", {silent:true});
+    }
+    
     const dirDepth = packageDir.split("/").length;
     for (let i = 0; i < dirDepth; i++) {
       shell.cd("..");
