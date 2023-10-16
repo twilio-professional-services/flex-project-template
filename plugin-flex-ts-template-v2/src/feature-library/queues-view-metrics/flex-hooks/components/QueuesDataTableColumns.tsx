3826222ab03d47b7a52464a6202b8c63f160d8c7
Ludo Vlieger <lvlieger@twilio.com>
2023-10-16 11:51:39 -0400

    Column description only as of 2.3

--- plugin-flex-ts-template-v2/src/feature-library/queues-view-metrics/flex-hooks/components/QueuesDataTableColumns.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/queues-view-metrics/flex-hooks/components/QueuesDataTableColumns.tsx
@@ -5,37 +5,68 @@ import { WorkerQueue } from '@twilio/flex-ui/src/state/QueuesState';
 import { FlexComponent } from '../../../../types/feature-loader';
 import { isAssignedTasksColumnEnabled, isWrappingTasksColumnEnabled } from '../../config';
 import { StringTemplates } from '../strings';
+import { isColumnDescriptionSupported } from '../../utils/helpers';
 
 export const componentName = FlexComponent.QueueStats;
 export const componentHook = function addQueuesDataTableColumns(flex: typeof Flex, manager: Flex.Manager) {
   if (isAssignedTasksColumnEnabled()) {
-    flex.QueuesStats.QueuesDataTable.Content.add(
-      <flex.ColumnDefinition
-        key="assigned-tasks"
-        header={manager.strings.TaskAssigned}
-        subHeader={manager.strings.QueuesStatsSubHeaderNow}
-        description={(manager.strings as any)[StringTemplates.AssignedTasksMetric]}
-        content={(queue: WorkerQueue) => {
-          const assignedTasks = queue.tasks_by_status?.assigned || 0;
-          return <span>{assignedTasks}</span>;
-        }}
-      />,
-      { sortOrder: 0 },
-    );
+    if (isColumnDescriptionSupported()) {
+      flex.QueuesStats.QueuesDataTable.Content.add(
+        <flex.ColumnDefinition
+          key="assigned-tasks"
+          header={manager.strings.TaskAssigned}
+          subHeader={manager.strings.QueuesStatsSubHeaderNow}
+          description={(manager.strings as any)[StringTemplates.AssignedTasksMetric]}
+          content={(queue: WorkerQueue) => {
+            const assignedTasks = queue.tasks_by_status?.assigned || 0;
+            return <span>{assignedTasks}</span>;
+          }}
+        />,
+        { sortOrder: 0 },
+      );
+    } else {
+      flex.QueuesStats.QueuesDataTable.Content.add(
+        <flex.ColumnDefinition
+          key="assigned-tasks"
+          header={manager.strings.TaskAssigned}
+          subHeader={manager.strings.QueuesStatsSubHeaderNow}
+          content={(queue: WorkerQueue) => {
+            const assignedTasks = queue.tasks_by_status?.assigned || 0;
+            return <span>{assignedTasks}</span>;
+          }}
+        />,
+        { sortOrder: 0 },
+      );
+    }
   }
   if (isWrappingTasksColumnEnabled()) {
-    flex.QueuesStats.QueuesDataTable.Content.add(
-      <flex.ColumnDefinition
-        key="wrapping-tasks"
-        header={manager.strings.TaskWrapup}
-        subHeader={manager.strings.QueuesStatsSubHeaderNow}
-        description={(manager.strings as any)[StringTemplates.WrappingTasksMetric]}
-        content={(queue: WorkerQueue) => {
-          const wrappingTasks = queue.tasks_by_status?.wrapping || 0;
-          return <span>{wrappingTasks}</span>;
-        }}
-      />,
-      { sortOrder: 0 },
-    );
+    if (isColumnDescriptionSupported()) {
+      flex.QueuesStats.QueuesDataTable.Content.add(
+        <flex.ColumnDefinition
+          key="wrapping-tasks"
+          header={manager.strings.TaskWrapup}
+          subHeader={manager.strings.QueuesStatsSubHeaderNow}
+          description={(manager.strings as any)[StringTemplates.WrappingTasksMetric]}
+          content={(queue: WorkerQueue) => {
+            const wrappingTasks = queue.tasks_by_status?.wrapping || 0;
+            return <span>{wrappingTasks}</span>;
+          }}
+        />,
+        { sortOrder: 0 },
+      );
+    } else {
+      flex.QueuesStats.QueuesDataTable.Content.add(
+        <flex.ColumnDefinition
+          key="wrapping-tasks"
+          header={manager.strings.TaskWrapup}
+          subHeader={manager.strings.QueuesStatsSubHeaderNow}
+          content={(queue: WorkerQueue) => {
+            const wrappingTasks = queue.tasks_by_status?.wrapping || 0;
+            return <span>{wrappingTasks}</span>;
+          }}
+        />,
+        { sortOrder: 0 },
+      );
+    }
   }
 };
