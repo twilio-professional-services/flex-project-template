c34164e81adc46db74bd0a5c7cf827b2f1caf3a4
Sam Johnson <sam@scj.me>
2023-10-16 14:46:36 -0500

    Adjust version check

--- plugin-flex-ts-template-v2/src/feature-library/queues-view-metrics/flex-hooks/components/QueuesDataTableColumns.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/queues-view-metrics/flex-hooks/components/QueuesDataTableColumns.tsx
@@ -10,63 +10,41 @@ import { isColumnDescriptionSupported } from '../../utils/helpers';
 export const componentName = FlexComponent.QueueStats;
 export const componentHook = function addQueuesDataTableColumns(flex: typeof Flex, manager: Flex.Manager) {
   if (isAssignedTasksColumnEnabled()) {
+    const props: any = {};
     if (isColumnDescriptionSupported()) {
-      flex.QueuesStats.QueuesDataTable.Content.add(
-        <flex.ColumnDefinition
-          key="assigned-tasks"
-          header={manager.strings.TaskAssigned}
-          subHeader={manager.strings.QueuesStatsSubHeaderNow}
-          description={(manager.strings as any)[StringTemplates.AssignedTasksMetric]}
-          content={(queue: WorkerQueue) => {
-            const assignedTasks = queue.tasks_by_status?.assigned || 0;
-            return <span>{assignedTasks}</span>;
-          }}
-        />,
-        { sortOrder: 0 },
-      );
-    } else {
-      flex.QueuesStats.QueuesDataTable.Content.add(
-        <flex.ColumnDefinition
-          key="assigned-tasks"
-          header={manager.strings.TaskAssigned}
-          subHeader={manager.strings.QueuesStatsSubHeaderNow}
-          content={(queue: WorkerQueue) => {
-            const assignedTasks = queue.tasks_by_status?.assigned || 0;
-            return <span>{assignedTasks}</span>;
-          }}
-        />,
-        { sortOrder: 0 },
-      );
+      props.description = (manager.strings as any)[StringTemplates.AssignedTasksMetric];
     }
+    flex.QueuesStats.QueuesDataTable.Content.add(
+      <flex.ColumnDefinition
+        key="assigned-tasks"
+        header={manager.strings.TaskAssigned}
+        subHeader={manager.strings.QueuesStatsSubHeaderNow}
+        content={(queue: WorkerQueue) => {
+          const assignedTasks = queue.tasks_by_status?.assigned || 0;
+          return <span>{assignedTasks}</span>;
+        }}
+        {...props}
+      />,
+      { sortOrder: 0 },
+    );
   }
   if (isWrappingTasksColumnEnabled()) {
+    const props: any = {};
     if (isColumnDescriptionSupported()) {
-      flex.QueuesStats.QueuesDataTable.Content.add(
-        <flex.ColumnDefinition
-          key="wrapping-tasks"
-          header={manager.strings.TaskWrapup}
-          subHeader={manager.strings.QueuesStatsSubHeaderNow}
-          description={(manager.strings as any)[StringTemplates.WrappingTasksMetric]}
-          content={(queue: WorkerQueue) => {
-            const wrappingTasks = queue.tasks_by_status?.wrapping || 0;
-            return <span>{wrappingTasks}</span>;
-          }}
-        />,
-        { sortOrder: 0 },
-      );
-    } else {
-      flex.QueuesStats.QueuesDataTable.Content.add(
-        <flex.ColumnDefinition
-          key="wrapping-tasks"
-          header={manager.strings.TaskWrapup}
-          subHeader={manager.strings.QueuesStatsSubHeaderNow}
-          content={(queue: WorkerQueue) => {
-            const wrappingTasks = queue.tasks_by_status?.wrapping || 0;
-            return <span>{wrappingTasks}</span>;
-          }}
-        />,
-        { sortOrder: 0 },
-      );
+      props.description = (manager.strings as any)[StringTemplates.WrappingTasksMetric];
     }
+    flex.QueuesStats.QueuesDataTable.Content.add(
+      <flex.ColumnDefinition
+        key="wrapping-tasks"
+        header={manager.strings.TaskWrapup}
+        subHeader={manager.strings.QueuesStatsSubHeaderNow}
+        content={(queue: WorkerQueue) => {
+          const wrappingTasks = queue.tasks_by_status?.wrapping || 0;
+          return <span>{wrappingTasks}</span>;
+        }}
+        {...props}
+      />,
+      { sortOrder: 0 },
+    );
   }
 };
