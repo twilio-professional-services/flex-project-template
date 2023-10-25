157fd09a50aef8a15b45933aae171c08be2609f6
Ludo Vlieger <lvlieger@twilio.com>
2023-10-25 15:52:11 -0400

    Remove worker_canvas_tabs config. Enable always

--- plugin-flex-ts-template-v2/src/feature-library/worker-canvas-tabs/config.ts
+++ plugin-flex-ts-template-v2/src/feature-library/worker-canvas-tabs/config.ts
@@ -1,15 +1,9 @@
 import { getFeatureFlags } from '../../utils/configuration';
-import WorkerCanvasTabsConfig from './types/ServiceConfiguration';
-
-const { enabled = false } = (getFeatureFlags()?.features?.worker_canvas_tabs as WorkerCanvasTabsConfig) || {};
 
 const { enabled: supervisorCapacityEnabled = false } = getFeatureFlags()?.features?.supervisor_capacity || {};
 const { enabled: attributeViewerEnabled = false } = getFeatureFlags()?.features?.attribute_viewer || {};
 const { enabled: workerDetailsEnabled = false } = getFeatureFlags()?.features?.worker_details || {};
 
-export const isFeatureEnabled = () => {
-  return enabled;
-};
 export const isSupervisorCapacityEnabled = () => {
   return supervisorCapacityEnabled;
 };
--- plugin-flex-ts-template-v2/src/feature-library/worker-canvas-tabs/types/ServiceConfiguration.ts
+++
@@ -1,3 +0,0 @@
-export default interface WorkerCanvasTabsConfig {
-  enabled: boolean;
-}
