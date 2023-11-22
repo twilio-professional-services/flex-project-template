c53c3ad0ec9cd4672f076153c1b7d6754f913ca6
Ludo Vlieger <lvlieger@twilio.com>
2023-11-22 12:42:47 -0500

    Update Outbound Call modal

--- plugin-flex-ts-template-v2/src/feature-library/contact-history/custom-components/OutboundCallModal.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/contact-history/custom-components/OutboundCallModal.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
-import { Actions, Template, templates } from '@twilio/flex-ui';
+import { Manager, Actions, Template, templates } from '@twilio/flex-ui';
 import { Flex } from '@twilio-paste/core/flex';
 import { Box } from '@twilio-paste/core/box';
+import { Heading } from '@twilio-paste/core/heading';
+import { Text } from '@twilio-paste/core/text';
 import { Button } from '@twilio-paste/core/button';
+import { Separator } from '@twilio-paste/core/separator';
 import { useUID } from '@twilio-paste/core/uid-library';
 import { Modal, ModalBody, ModalFooter, ModalFooterActions, ModalHeader, ModalHeading } from '@twilio-paste/core/modal';
 import { CallOutgoingIcon } from '@twilio-paste/icons/esm/CallOutgoingIcon';
 
+import { CustomWorkerAttributes } from '../../../types/task-router/Worker';
 import { StringTemplates } from '../flex-hooks/strings';
 
 const placeOutboundCall = async (destination: string) => {
@@ -23,6 +27,8 @@ const OutboundCallModal = ({ phoneNumber }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
   const handleClose = () => setIsOpen(false);
   const modalHeadingID = useUID();
+
+  const { selectedCallerId } = Manager.getInstance().workerClient?.attributes as CustomWorkerAttributes;
 
   return (
     <div>
@@ -51,7 +57,25 @@ const OutboundCallModal = ({ phoneNumber }: Props) => {
           </ModalHeading>
         </ModalHeader>
         <ModalBody>
-          <Template source={templates[StringTemplates.OutboundCallDialog]} />: {phoneNumber}
+          <Flex>
+            <Text as="p">
+              <Template source={templates[StringTemplates.OutboundCallDialog]} />:
+            </Text>
+          </Flex>
+          <Flex>
+            <Heading as="h3" variant="heading30">
+              {phoneNumber}
+            </Heading>
+          </Flex>
+          <Separator orientation="horizontal" verticalSpacing="space50" />
+          <Flex>
+            <Text as="p">
+              <Template source={templates[StringTemplates.OutboundCallerId]} />:
+            </Text>
+          </Flex>
+          <Flex>
+            <Text as="p">{selectedCallerId}</Text>
+          </Flex>
         </ModalBody>
         <ModalFooter>
           <ModalFooterActions>
