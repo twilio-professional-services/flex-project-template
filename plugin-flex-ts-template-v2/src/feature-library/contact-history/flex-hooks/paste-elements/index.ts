a13338c51ac01c6173d2b3e45ab44b9eb554f553
Ludo Vlieger <75390074+ldvlgr@users.noreply.github.com>
2023-12-12 13:08:03 -0500

    Contact History view (#437)
    Co-authored-by: Sam Johnson <7373633+dremin@users.noreply.github.com>
    Co-authored-by: Sam Johnson <sam@scj.me>

---
+++ plugin-flex-ts-template-v2/src/feature-library/contact-history/flex-hooks/paste-elements/index.ts
@@ -0,0 +1,7 @@
+import { PasteCustomCSS } from '@twilio-paste/customization';
+
+export const pasteElementHook = {
+  COMPACT_MODAL: {
+    width: '400px',
+  },
+} as { [key: string]: PasteCustomCSS };
