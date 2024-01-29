31bc9d5d7de62b5630a62e070191f127a524772e
Sam Johnson <sam@scj.me>
2024-01-29 11:06:23 -0600

    Don’t use terraform console

--- infra-as-code/state/import_internal_state.sh
+++ infra-as-code/state/import_internal_state.sh
@@ -36,13 +36,10 @@ import_resource() {
 importInternalState() {
 	echo "aa2a"
 	echo " - Discovering and importing existing Twilio state for known definitions into a new terraform state file" >>$GITHUB_STEP_SUMMARY
-	echo "var.TWILIO_FLEX_WORKSPACE_SID" | terraform -chdir="../terraform/environments/default" console -input=false -var-file="${ENVIRONMENT:-local}.tfvars" | tr -d '"'
-	# TF_WORKSPACE_SID=$(echo "var.TWILIO_FLEX_WORKSPACE_SID" | terraform -chdir="../terraform/environments/default" console -input=false -var-file="${ENVIRONMENT:-local}.tfvars" | tr -d '"')
-	TF_WORKSPACE_SID="test"
+	TF_WORKSPACE_SID=$(cat "../terraform/environments/default/${ENVIRONMENT:-local}.tfvars" | grep "TWILIO_FLEX_WORKSPACE_SID" | sed 's/ = /=/;s/^\([^ ]*\)="\([^"].*\)"/\2/')
 	echo "aa2b"
 
 	workflows=$(npx twilio api:taskrouter:v1:workspaces:workflows:list --workspace-sid "$TF_WORKSPACE_SID" --no-limit -o json | jq 'map(del(.configuration))')
-	echo "aa2c"
 	queues=$(npx twilio api:taskrouter:v1:workspaces:task-queues:list --workspace-sid "$TF_WORKSPACE_SID" --no-limit -o json)
 	channels=$(npx twilio api:taskrouter:v1:workspaces:task-channels:list --workspace-sid "$TF_WORKSPACE_SID" --no-limit -o json)
 	activities=$(npx twilio api:taskrouter:v1:workspaces:activities:list --workspace-sid "$TF_WORKSPACE_SID" --no-limit -o json)
@@ -97,16 +94,13 @@ importInternalState() {
 
 # populate tfvars
 (cd ../.. && npm run postinstall -- --skip-packages --files=infra-as-code/terraform/environments/default/example.tfvars)
-echo "aa1"
+
 ### only if existing state file does not exist
 ### do we want to import the internal state
 if ! [ -f ../terraform/environments/default/terraform.tfstate ]; then
-	echo "aa2"
   importInternalState
 fi
-echo "aa3"
 
 terraform -chdir="../terraform/environments/default" apply -input=false -auto-approve -var-file="${ENVIRONMENT:-local}.tfvars"
-echo "aa4"
 echo " - Applying terraform configuration complete" >>$GITHUB_STEP_SUMMARY
 echo "JOB_FAILED=false" >>"$GITHUB_OUTPUT"
