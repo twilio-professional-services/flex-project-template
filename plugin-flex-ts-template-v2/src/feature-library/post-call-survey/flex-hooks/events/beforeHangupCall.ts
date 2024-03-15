d2ba3d6d73f970dfc7f7f5bcd61b377af9dea602
Christopher Connolly <425070+chaosloth@users.noreply.github.com>
2024-03-15 22:43:38 +1100

    Cleanup service names

--- plugin-flex-ts-template-v2/src/feature-library/post-call-survey/flex-hooks/events/beforeHangupCall.ts
+++ plugin-flex-ts-template-v2/src/feature-library/post-call-survey/flex-hooks/events/beforeHangupCall.ts
@@ -2,46 +2,35 @@ import * as Flex from '@twilio/flex-ui';
 import { TaskHelper } from '@twilio/flex-ui';
 
 import { FlexAction, FlexActionEvent } from '../../../../types/feature-loader';
-import service from '../../utils/SurveyService';
+import surveyService from '../../utils/SurveyService';
 
 export const actionEvent = FlexActionEvent.before;
 export const actionName = FlexAction.HangupCall;
-export const actionHook = async (
-  flex: typeof Flex,
-  manager: Flex.Manager,
-  task: Flex.ITask
-) => {
-  flex.Actions.addListener(
-    `${actionEvent}${actionName}`,
-    async (payload, abortFunction) => {
-      const { task } = payload;
-      if (!TaskHelper.isCallTask(task) || TaskHelper.isOutboundCallTask(task)) {
-        return;
-      }
+export const actionHook = async (flex: typeof Flex, manager: Flex.Manager, task: Flex.ITask) => {
+  flex.Actions.addListener(`${actionEvent}${actionName}`, async (payload, abortFunction) => {
+    const { task } = payload;
+    if (!TaskHelper.isCallTask(task) || TaskHelper.isOutboundCallTask(task)) {
+      return;
+    }
 
-      console.log('post-call-survey', task);
+    console.log('post-call-survey', task);
 
-      const rules = await service.getRules();
+    const rules = await surveyService.getRules();
 
-      const surveyRequired = rules.find(
-        (r) => r.data.queue_name === task.queueName
-      );
+    const surveyRequired = rules.find((r) => r.data.queue_name === task.queueName);
 
-      if (surveyRequired) {
-        console.log(
-          `post-call-survey running survey: ${surveyRequired.data.survey_key}`
+    if (surveyRequired) {
+      console.log(`post-call-survey running survey: ${surveyRequired.data.survey_key}`);
+      try {
+        await surveyService.startSurvey(
+          surveyRequired.data.queue_name,
+          task.attributes.call_sid,
+          task.taskSid,
+          surveyRequired.data.survey_key,
         );
-        try {
-          await service.startSurvey(
-            surveyRequired.data.queue_name,
-            task.attributes.call_sid,
-            task.taskSid,
-            surveyRequired.data.survey_key
-          );
-        } catch (error) {
-          console.log('Error starting survey: ', error);
-        }
+      } catch (error) {
+        console.log('Error starting survey: ', error);
       }
     }
-  );
+  });
 };
