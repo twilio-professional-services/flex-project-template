563c8b869306bd6fdda923efdf81dcf05dda68d2
Sam Johnson <sam@scj.me>
2024-04-24 17:08:27 -0500

    Improve dispositions state handling
    Fix issues when handling multiple tasks
    Fix validation of required properties on CompleteTask

--- plugin-flex-ts-template-v2/src/feature-library/dispositions/flex-hooks/actions/CompleteTask.ts
+++ plugin-flex-ts-template-v2/src/feature-library/dispositions/flex-hooks/actions/CompleteTask.ts
@@ -40,24 +40,43 @@ export const actionHook = function setDispositionBeforeCompleteTask(flex: typeof
     const textAttributes = getTextAttributes(queueSid, queueName);
     const selectAttributes = getSelectAttributes(queueSid, queueName);
 
-    // If notes disabled, and no dispositions are configured, return.
+    // If notes are disabled, and no dispositions are configured, return.
     if (numDispositions < 1 && !isNotesEnabled() && textAttributes.length < 1 && selectAttributes.length < 1) {
       return;
     }
 
-    // First, check if a disposition and/or notes are set.
+    // Retrieve the user input from Redux.
     const { tasks } = (manager.store.getState() as AppState)[reduxNamespace].dispositions as DispositionsState;
+    const taskDisposition = tasks[payload.task.taskSid];
 
-    if (!tasks || !tasks[payload.task.taskSid]) {
+    // Check for any required custom attributes that are missing.
+    let missingCustomAttrs = 0;
+    textAttributes.forEach((attr) => {
+      if (
+        attr.required &&
+        (!taskDisposition?.custom_attributes || !taskDisposition?.custom_attributes[attr.conversation_attribute])
+      )
+        missingCustomAttrs += 1;
+    });
+    selectAttributes.forEach((attr) => {
+      if (
+        attr.required &&
+        (!taskDisposition?.custom_attributes || !taskDisposition?.custom_attributes[attr.conversation_attribute])
+      )
+        missingCustomAttrs += 1;
+    });
+
+    // If nothing exists for this task in Redux, we only need to check the configuration to see if we should abort.
+    if (!taskDisposition) {
       if (isRequireDispositionEnabledForQueue(queueSid, queueName) && numDispositions > 0) {
         handleAbort(flex, abortFunction, true);
+      } else if (missingCustomAttrs > 0) {
+        handleAbort(flex, abortFunction, false);
       }
       return;
     }
 
-    const taskDisposition = tasks[payload.task.taskSid];
-    let newConvAttributes = {};
-
+    // Validate the task data from Redux against the configuration to see if any required data is missing.
     if (
       isRequireDispositionEnabledForQueue(queueSid, queueName) &&
       !taskDisposition.disposition &&
@@ -66,14 +85,11 @@ export const actionHook = function setDispositionBeforeCompleteTask(flex: typeof
       handleAbort(flex, abortFunction, true);
       return;
     }
-    let missing = 0;
-    textAttributes.forEach((attr) => {
-      if (attr.required && !taskDisposition?.custom_attributes[attr.conversation_attribute]) missing += 1;
-    });
-    selectAttributes.forEach((attr) => {
-      if (attr.required && !taskDisposition?.custom_attributes[attr.conversation_attribute]) missing += 1;
-    });
-    if (missing > 0) handleAbort(flex, abortFunction, false);
+
+    if (missingCustomAttrs > 0) {
+      handleAbort(flex, abortFunction, false);
+      return;
+    }
 
     if (
       !taskDisposition.disposition &&
@@ -83,6 +99,9 @@ export const actionHook = function setDispositionBeforeCompleteTask(flex: typeof
       // Nothing for us to do, and it's okay!
       return;
     }
+
+    // We have data to save! Build the task attribute update payload.
+    let newConvAttributes = {};
 
     if (taskDisposition.disposition) {
       newConvAttributes = {
