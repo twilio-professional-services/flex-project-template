9e1efa7d5d741f685863aab44bc3b100063eb01f
Sam Johnson <sam@scj.me>
2024-06-03 17:55:25 -0500

    Expand a bit the mocked flex-ui for tests

--- plugin-flex-ts-template-v2/__mocks__/@twilio/flex-ui.js
+++ plugin-flex-ts-template-v2/__mocks__/@twilio/flex-ui.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import { EventEmitter } from 'events';
 import { getMockedServiceConfiguration, getMockedUiAttributes } from '../../test-utils/flex-service-configuration';
 import { getMockedReduxState } from '../../test-utils/flex-redux';
@@ -9,9 +10,7 @@ class WorkerClient extends EventEmitter {
   constructor() {
     super();
     this.sid = 'mockWorkerSid';
-    this.attributes = {
-
-    };
+    this.attributes = {};
     this.reservations = new Map();
   }
 }
@@ -20,7 +19,7 @@ class Manager {
   get serviceConfiguration() {
     return getMockedServiceConfiguration();
   }
-  
+
   get configuration() {
     return getMockedUiAttributes();
   }
@@ -28,13 +27,21 @@ class Manager {
   constructor() {
     this.events = new EventEmitter();
     this.workerClient = new WorkerClient();
+    this.conversationsClient = {
+      getConversationBySid: () =>
+        Promise.resolve({
+          attributes: {
+            associatedTasks: { TAxxx: '' },
+          },
+        }),
+    };
     this.store = {
       addReducer: jest.fn(),
       dispatch: jest.fn(),
-      getState: jest.fn(() => getMockedReduxState())
+      getState: jest.fn(() => getMockedReduxState()),
     };
     this.user = {
-      token: 'mockedToken'
+      token: 'mockedToken',
     };
   }
   getInstance() {
@@ -58,17 +65,19 @@ class Actions {
       const lifecycle = [
         this.eventListeners[`before${name}`] || [],
         this.eventListeners[name] || [],
-        this.eventListeners[`after${name}`] || []
+        this.eventListeners[`after${name}`] || [],
       ];
       try {
-        await Promise.all(lifecycle[0].map(listener => {
-          return new Promise(async (resolve, reject) => {
-            await listener(payload, () => {
-              reject(`Action ${name} cancelled by before event`);
+        await Promise.all(
+          lifecycle[0].map((listener) => {
+            return new Promise(async (resolve, reject) => {
+              await listener(payload, () => {
+                reject(`Action ${name} cancelled by before event`);
+              });
+              resolve();
             });
-            resolve();
-          })
-        }));
+          }),
+        );
       } catch (e) {
         throw new Error(e);
       }
@@ -84,14 +93,33 @@ class Actions {
       }
     });
 
-    this.removeAllListeners = jest.fn(event => {
+    this.removeAllListeners = jest.fn((event) => {
       if (event) {
         delete this.eventListeners[event];
       } else {
         this.eventListeners = {};
       }
     });
+
+    this.removeListener = jest.fn((event, listener) => {
+      if (this.eventListeners[event]) {
+        delete this.eventListeners[event][listener];
+      } else {
+        this.eventListeners[event] = [];
+      }
+    });
   }
+}
+
+class TaskHelper {
+  constructor() {}
+
+  canHold = jest.fn().mockReturnValue(true);
+  isOutboundCallTask = jest.fn().mockReturnValue(true);
+  isCallTask = jest.fn().mockReturnValue(true);
+  isChatBasedTask = jest.fn().mockReturnValue(true);
+  isCBMTask = jest.fn().mockReturnValue(false);
+  getTaskByTaskSid = jest.fn();
 }
 
 class Notifications {
@@ -107,70 +135,121 @@ class Notifications {
   dismissNotificationById = jest.fn();
 }
 
+class DynamicContentStore {
+  add = jest.fn();
+  remove = jest.fn();
+  replace = jest.fn();
+}
+
 module.exports = {
   CRMContainer: {
-    Content: {
-      replace: jest.fn()
-    }
+    Content: new DynamicContentStore(),
+  },
+  CallCanvas: {
+    Content: new DynamicContentStore(),
+  },
+  TaskCanvasHeader: {
+    Content: new DynamicContentStore(),
+  },
+  TaskCanvasTabs: {
+    Content: new DynamicContentStore(),
+  },
+  CallCanvasActions: {
+    Content: new DynamicContentStore(),
+  },
+  ParticipantsCanvas: {
+    Content: new DynamicContentStore(),
+  },
+  ParticipantCanvas: {
+    Content: new DynamicContentStore(),
+    ListItem: {
+      Content: new DynamicContentStore(),
+    },
   },
   AgentDesktopView: {
     defaultProps: {
-      splitterOptions: {}
-    }
+      splitterOptions: {},
+    },
+  },
+  WorkerDirectory: {
+    Tabs: {
+      Content: new DynamicContentStore(),
+    },
   },
   Actions: new Actions(),
   Manager: new Manager(),
   Notifications: new Notifications(),
+  TaskHelper: new TaskHelper(),
   NotificationType: {
     warning: 'warning',
-    error: 'error'
+    error: 'error',
   },
-  templates: {
-    WorkerDirectorySearchPlaceholder: () => '',
-    WarmTransferTooltip: () => '',
-    ColdTransferTooltip: () => ''
-  },
+  templates: new Proxy(
+    {},
+    {
+      get(_target, p) {
+        return () => p;
+      },
+    },
+  ),
   NotificationBar: {
-    Action: () => <React.Fragment></React.Fragment>
+    Action: () => <React.Fragment></React.Fragment>,
   },
+  ChatOrchestrator: {
+    orchestrateCompleteTask: jest.fn(),
+  },
+  StateHelper: {
+    getTaskByTaskrouterTaskSid: (props) => jest.fn(),
+    getConversationStateForTask: jest.fn(),
+  },
+  styled: styled,
+  Template: ({ source }) => <span>{source()}</span>,
   SideLink: () => ({
     render() {
       return <React.Fragment />;
-    }
+    },
   }),
+  Button: ({ children, ...props }) => <button {...props}>{children}</button>,
   Icon: () => ({
     render() {
       return <React.Fragment />;
-    }
+    },
   }),
-  IconButton: () => ({
-    render() {
-      return <React.Fragment />;
-    }
-  }),
+  IconButton: (props) => <button {...props} />,
   FlexBox: () => ({
     render() {
       return <React.Fragment />;
-    }
+    },
   }),
   FlexBoxColumn: () => ({
     render() {
       return <React.Fragment />;
-    }
+    },
+  }),
+  Tab: () => ({
+    render() {
+      return <React.Fragment />;
+    },
   }),
   ConferenceParticipant: ConferenceParticipant,
+  Plugins: {
+    plugins: [],
+  },
   withTaskContext: (WrappedComponent) => {
     return () => ({
       render() {
         return <WrappedComponent />;
-      }
+      },
     });
+  },
+  TaskContext: {
+    Consumer: React.Fragment,
   },
   withTheme: (WrappedComponent) => {
     return () => ({
       render() {
         return <WrappedComponent theme={{}} />;
-      }
+      },
     });
-  }
+  },
 };
