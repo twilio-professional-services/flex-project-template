560166e9a6e43d0f7d7abdf64f0d5e62e3cf8a60
CJB <chris.berno@gmail.com>
2025-06-09 14:43:13 -0400

    updats to consume first/last name in task queue

--- plugin-flex-ts-template-v2/src/FlexTSTemplatePlugin.tsx
+++ plugin-flex-ts-template-v2/src/FlexTSTemplatePlugin.tsx
@@ -12,11 +12,13 @@ export default class FlexTSTemplatePlugin extends FlexPlugin {
     // This disables type-checking for the replace function but WORKS
     (flex.TaskListItem.Content as any).replace((props: any) => {
       const { task } = props;
-      console.log('Task attributes:', task.attributes); // Debug log
-      const { firstname, lastname } = (task.attributes ?? {}) as {
-        firstname?: string;
-        lastname?: string;
-      };
+      console.log('Task attributes:', JSON.stringify(task.attributes, null, 2)); // More detailed logging
+      
+      // Access the profile data correctly based on how it's passed from Studio
+      const profile = task.attributes.profile || {};
+      const firstname = profile.firstname || '';
+      const lastname = profile.lastname || '';
+      
       const name = [firstname, lastname].filter(Boolean).join(' ') || 'Unknown Caller';
 
       return (
