1f58b8760442a77458497c398fc02481b6cbfa71
CJB <chris.berno@gmail.com>
2025-06-09 18:18:27 -0400

    more plugin updates for displaying name attributes

--- plugin-flex-ts-template-v2/src/FlexTSTemplatePlugin.tsx
+++ plugin-flex-ts-template-v2/src/FlexTSTemplatePlugin.tsx
@@ -1,5 +1,6 @@
 import * as Flex from '@twilio/flex-ui';
 import { FlexPlugin } from '@twilio/flex-plugin';
+import React from 'react';
 
 const PLUGIN_NAME = 'FlexTSTemplatePlugin';
 
@@ -9,23 +10,16 @@ export default class FlexTSTemplatePlugin extends FlexPlugin {
   }
 
   init(flex: typeof Flex, manager: Flex.Manager) {
-    // This disables type-checking for the replace function but WORKS
-    (flex.TaskListItem.Content as any).replace((props: any) => {
-      const { task } = props;
-      console.log('Task attributes:', JSON.stringify(task.attributes, null, 2)); // More detailed logging
+    // Use a simpler approach to customize the TaskListItem
+    // Use type assertion to bypass TypeScript error
+    (flex.TaskListItem.defaultProps as any).firstLine = (task: Flex.ITask) => {
+      console.log('Task attributes:', JSON.stringify(task.attributes, null, 2));
       
-      // Access the profile data correctly based on how it's passed from Studio
-      const profile = task.attributes.profile || {};
-      const firstname = profile.firstname || '';
-      const lastname = profile.lastname || '';
+      const firstname = task.attributes.firstname || '';
+      const lastname = task.attributes.lastname || '';
+      const name = [firstname, lastname].filter(Boolean).join(' ');
       
-      const name = [firstname, lastname].filter(Boolean).join(' ') || 'Unknown Caller';
-
-      return (
-        <div>
-          <strong>{name}</strong>
-        </div>
-      );
-    });
+      return name || task.defaultFrom || 'Unknown Caller';
+    };
   }
 }
