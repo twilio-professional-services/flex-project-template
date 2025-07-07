22849e6d08ecf8d69356814450f27f10c5c85da0
chrisberno <chris.berno@gmail.com>
2025-07-07 10:20:31 -0400

    Configure custom domain: Update URLs for docs.connie.one
    - Update Docusaurus config for custom domain
    - Change baseUrl from /connieRTC-flex/ to /
    - Fix all killer box button links for new URL structure
    - Site will be available at https://docs.connie.one once TLS cert is ready

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -10,10 +10,10 @@ const config = {
   favicon: "img/logos/connie-rtc-icon.png",
 
   // Set the production url of your site here
-  url: "https://ConnieML.github.io",
+  url: "https://docs.connie.one",
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/connieRTC-flex/",
+  // For custom domains, this is usually just '/'
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
