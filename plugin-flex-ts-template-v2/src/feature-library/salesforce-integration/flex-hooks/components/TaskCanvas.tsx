bb2e3d0e5df62b8aae27263a13baabaeaa935a71
Sam Johnson <sam@scj.me>
2025-08-01 10:59:39 -0500

    Add TaskCanvas to components enum

--- plugin-flex-ts-template-v2/src/feature-library/salesforce-integration/flex-hooks/components/TaskCanvas.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/salesforce-integration/flex-hooks/components/TaskCanvas.tsx
@@ -3,8 +3,8 @@ import * as Flex from '@twilio/flex-ui';
 import AssociateRecordDropdown from '../../custom-components/AssociateRecordDropdown';
 import { FlexComponent } from '../../../../types/feature-loader';
 
-export const componentName = 'TaskCanvas'; // FlexComponent.TaskCanvas
-export const componentHook = function addAsssociateRecordDropdownToCanvas(flex: typeof Flex, manager: Flex.Manager) {
+export const componentName = FlexComponent.TaskCanvas;
+export const componentHook = function addAsssociateRecordDropdownToCanvas(flex: typeof Flex) {
   flex.TaskCanvas.Content.add(<AssociateRecordDropdown key="associate-record-dropdown" />, {
     sortOrder: 1,
   });
