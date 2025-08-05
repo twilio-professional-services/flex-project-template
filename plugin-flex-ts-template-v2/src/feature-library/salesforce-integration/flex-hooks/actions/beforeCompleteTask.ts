66614cc42acb68c42ae2192801107d1ec77b1b64
Sam Johnson <sam@scj.me>
2025-08-04 19:39:50 -0500

    Perform less work in certain configurations

--- plugin-flex-ts-template-v2/src/feature-library/salesforce-integration/flex-hooks/actions/beforeCompleteTask.ts
+++ plugin-flex-ts-template-v2/src/feature-library/salesforce-integration/flex-hooks/actions/beforeCompleteTask.ts
@@ -3,7 +3,7 @@ import * as Flex from '@twilio/flex-ui';
 import { FlexActionEvent, FlexAction } from '../../../../types/feature-loader';
 import { getOpenCti } from '../../utils/SfdcHelper';
 import logger from '../../../../utils/logger';
-import { isActivityLoggingEnabled } from '../../config';
+import { isActivityLoggingEnabled, isScreenPopEnabled } from '../../config';
 import { reduxNamespace } from '../../../../utils/state';
 import { AppState } from '../../../../types/manager';
 import { SalesforceIntegrationNotification } from '../notifications';
@@ -12,7 +12,7 @@ export const actionEvent = FlexActionEvent.before;
 export const actionName = FlexAction.CompleteTask;
 export const actionHook = function checkRecordAssociation(flex: typeof Flex, manager: Flex.Manager) {
   flex.Actions.addListener(`${actionEvent}${actionName}`, async (payload, abortFunction) => {
-    if (!isActivityLoggingEnabled() || !getOpenCti()) {
+    if (!isActivityLoggingEnabled() || !isScreenPopEnabled() || !getOpenCti()) {
       return;
     }
 
--- plugin-flex-ts-template-v2/src/feature-library/salesforce-integration/flex-hooks/components/TaskCanvas.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/salesforce-integration/flex-hooks/components/TaskCanvas.tsx
@@ -2,9 +2,13 @@ import * as Flex from '@twilio/flex-ui';
 
 import AssociateRecordDropdown from '../../custom-components/AssociateRecordDropdown';
 import { FlexComponent } from '../../../../types/feature-loader';
+import { isActivityLoggingEnabled, isScreenPopEnabled } from '../../config';
 
 export const componentName = FlexComponent.TaskCanvas;
 export const componentHook = function addAsssociateRecordDropdownToCanvas(flex: typeof Flex) {
+  if (!isActivityLoggingEnabled() || !isScreenPopEnabled()) {
+    return;
+  }
   flex.TaskCanvas.Content.add(<AssociateRecordDropdown key="associate-record-dropdown" />, {
     sortOrder: 1,
   });
