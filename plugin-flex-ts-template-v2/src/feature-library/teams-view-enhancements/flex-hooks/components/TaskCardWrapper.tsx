91f9813f76e7fd17af3a93d5e07db34ad68ebb7b
Sam Johnson <sam@scj.me>
2025-09-10 10:07:05 -0500

    Update teams view columns for Flex UI changes
    Removed obsolete options

--- plugin-flex-ts-template-v2/src/feature-library/teams-view-enhancements/flex-hooks/components/TaskCardWrapper.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/teams-view-enhancements/flex-hooks/components/TaskCardWrapper.tsx
@@ -3,19 +3,10 @@ import * as Flex from '@twilio/flex-ui';
 
 import { FlexComponent } from '../../../../types/feature-loader';
 import { TaskCardBox, TaskCardInnerBox } from './TaskCardWrapper.Styles';
-import {
-  isHTHighlightEnabled,
-  getHTWarningThreshold,
-  getHTExceededThreshold,
-  isDisplayTaskQueueNameEnabled,
-} from '../../config';
+import { isHTHighlightEnabled, getHTWarningThreshold, getHTExceededThreshold } from '../../config';
 
 export const componentName = FlexComponent.TeamsView;
 export const componentHook = function addTaskCardWrapper(flex: typeof Flex, manager: Flex.Manager) {
-  if (isDisplayTaskQueueNameEnabled()) {
-    // instead of {{task.defaultFrom}}, show the queue name on the Task Card
-    manager.strings.SupervisorTaskCardHeader = '{{task.queueName}}';
-  }
   if (isHTHighlightEnabled()) {
     flex.Supervisor.TaskCard.Content.addWrapper((Original) => (originalProps) => {
       const now = new Date();
