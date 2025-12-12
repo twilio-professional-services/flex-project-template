24385c220d34bc4e25b62fc55875ac7c9a14488c
Sam Johnson <7373633+dremin@users.noreply.github.com>
2025-12-12 12:23:22 -0600

    Update docusaurus (#810)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -21,7 +21,6 @@ const config = {
   projectName: "flex-project-template", // Usually your repo name.
   trailingSlash: false,
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
