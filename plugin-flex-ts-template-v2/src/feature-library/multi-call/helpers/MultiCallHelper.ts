93d7f577d8a6aae5d453d467894b3020ead9c47f
Sam Johnson <sam@scj.me>
2026-02-27 14:02:56 -0600

    Remove unnecessary condition

--- plugin-flex-ts-template-v2/src/feature-library/multi-call/helpers/MultiCallHelper.ts
+++ plugin-flex-ts-template-v2/src/feature-library/multi-call/helpers/MultiCallHelper.ts
@@ -121,10 +121,7 @@ export const handleUnhold = (payload: any) => {
 
 const updateCallState = (manager: Manager, endedCall: Call, device?: Device) => {
   if (MultiCallCalls.length < 1) {
-    if (
-      device &&
-      manager.store.getState().flex?.phone?.activeCall?.parameters.CallSid === endedCall.parameters.CallSid
-    ) {
+    if (device) {
       // The call that ended was not the Flex device, and no other calls are in progress, so the state needs to be updated.
       try {
         manager.store.dispatch({ type: 'PHONE_REMOVE_CALL' });
